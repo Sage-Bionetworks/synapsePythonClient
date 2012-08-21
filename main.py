001390fea5ea59bad654791cd3f5badf49466aad
lomberg <lom@larssono.com>
2012-08-21 08:37:51 -0700

    Removes build files. If needed someone will have to re-add.

--- main.py
+++
@@ -1,9 +0,0 @@
-from synapse.client import Synapse
-
-s = Synapse('https://repo-alpha.sagebase.org/repo/v1','https://auth-alpha.sagebase.org/auth/v1', 30, False)
-s.login('platform@sagebase.org', 'Grftac11')
-e = s.startRestore("BackupDaemonJob88445-857150449942082859.zip")
-#id = e["id"]
-#f = s.monitorDaemonStatus(88453)
-#print f
-
