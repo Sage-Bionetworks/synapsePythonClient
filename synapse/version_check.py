538058dca1a99c4d7b761d11a0a106fa9a8c77e0
Christopher Bare <chris.bare@sagebase.org>
2012-12-17 11:48:51 -0800

    read currently running version info from file synapsePythonClient

--- synapse/version_check.py
+++ synapse/version_check.py
@@ -10,41 +10,64 @@ import traceback
 import warnings
 
 
-CURRENT_VERSION = "0.1.1"
+version_info_filename = "synapsePythonClient"
 
 
-## Get the latest version information from version_url and check against
-## the current version. Recommend upgrade, if a newer version exists.
-def version_check(current_version=CURRENT_VERSION,
-				  version_url="http://versions.synapse.sagebase.org/synapsePythonClient",
-				  upgrade_url="https://github.com/Sage-Bionetworks/synapsePythonClient"):
 
-	try:
-		headers = { 'Accept': 'application/json' }
-		version_info = requests.get(version_url, headers=headers).json
+def read_version_info_file():
+    """
+    Read version info file and return a dictionary 
+    """
+    with open(version_info_filename) as f:
+        version_info = json.loads(f.read())
+    return version_info
 
-		## check blacklist
-		if current_version in version_info['blacklist']:
-			msg = "\nPLEASE UPGRADE YOUR CLIENT\n\nUpgrading your SynapseClient is required. Please visit:\n%s\n\n" % (upgrade_url,)
-			raise SystemExit(msg)
 
-		if 'message' in version_info:
-			sys.stdout.write(version_info['message'] + '\n')
+def getCurrentVersion():
+    return read_version_info_file()['latestVersion']
 
-		## check latest version
-		if StrictVersion(current_version) < StrictVersion(version_info['latestVersion']):
-			msg = "\nUPGRADE AVAILABLE\n\nA more recent version of the Synapse Client (%s) is available. Your version (%s) can be upgraded by visiting:\n%s\n\n" % (version_info['latestVersion'], current_version, upgrade_url,)
-			sys.stderr.write(msg)
-			if 'releaseNotes' in version_info:
-				sys.stdout.write(version_info['releaseNotes'] + '\n')
-			return False
 
-	except Exception, e:
-		## don't prevent the client from running if something goes wrong
-		sys.stderr.write("Exception in version check.\n")
-		return False
+def version_check(current_version=None,
+                  version_url="http://versions.synapse.sagebase.org/synapsePythonClient",
+                  upgrade_url="https://github.com/Sage-Bionetworks/synapsePythonClient"):
+    """
+    Get the latest version information from version_url and check against
+    the current version. Recommend upgrade, if a newer version exists.
+    """
 
-	return True
+    try:
+        if (not current_version):
+            current_version = getCurrentVersion()
 
+        headers = { 'Accept': 'application/json' }
+        version_info = requests.get(version_url, headers=headers).json
+
+        ## check blacklist
+        if current_version in version_info['blacklist']:
+            msg = "\nPLEASE UPGRADE YOUR CLIENT\n\nUpgrading your SynapseClient is required. Please visit:\n%s\n\n" % (upgrade_url,)
+            raise SystemExit(msg)
+
+        if 'message' in version_info:
+            sys.stdout.write(version_info['message'] + '\n')
+
+        ## check latest version
+        if StrictVersion(current_version) < StrictVersion(version_info['latestVersion']):
+            msg = "\nUPGRADE AVAILABLE\n\nA more recent version of the Synapse Client (%s) is available. Your version (%s) can be upgraded by visiting:\n%s\n\n" % (version_info['latestVersion'], current_version, upgrade_url,)
+            sys.stderr.write(msg)
+            if 'releaseNotes' in version_info:
+                sys.stdout.write(version_info['releaseNotes'] + '\n')
+            return False
+
+    except Exception, e:
+        ## don't prevent the client from running if something goes wrong
+        sys.stderr.write("Exception in version check.\n")
+        return False
+
+    return True
+
+## if this file is run as a script, print current version
+## then perform version check
 if __name__ == "__main__":
-    version_check()
+    print("Python Synapse Client version: %s" % (read_version_info_file()['latestVersion'],))
+    if version_check():
+        print("ok")
