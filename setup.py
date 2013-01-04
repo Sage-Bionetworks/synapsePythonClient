6206a18ab4173f9d3e5e45876ff14be84bf148e2
lomberg <lom@larssono.com>
2013-01-03 17:38:20 -0800

    Changes name of distutil package.

--- setup.py
+++ setup.py
@@ -3,38 +3,35 @@
 
 from setuptools import setup
 import json
+from synapseclient.client import __version__
 
-
-description = """A client for the Sage Synapse, a collaborative compute space 
+description = """A client for Synapse, a collaborative compute space 
 that allows scientists to share and analyze data together.""".replace("\n", " ")
 
-long_description = """A client for the Sage Synapse, a collaborative compute
-space that allows scientists to share and analyze data together. Synapse is an
-innovation space that brings together scientific data, tools, and disease
-models into a Commons that enables true collaborative research. The platform
-consists of a web portal, web services, and integration with data analysis tools.
+long_description = """A client for Synapse, a collaborative compute
+space that allows scientists to share and analyze data
+together. Synapse brings together scientific data, tools, and disease
+models into a commons that enables true collaborative research. The
+platform consists of a web portal, web services, and integration with
+data analysis tools such as R, python, Galaxy and Java.
 """.replace("\n", " ")
 
-## read current version in from JSON file
-with open("synapsePythonClient") as f:
-        version_info = json.loads(f.read())
-current_version = version_info['latestVersion']
 
-setup(name='SynapseClient',
-    version=current_version,
+setup(name='synapseclient',
+    version=__version__,
     description=description,
     long_description=long_description,
     url='http://synapse.sagebase.org/',
     download_url="https://github.com/Sage-Bionetworks/synapsePythonClient",
     author='Synapse Team',
     author_email='platform@sagebase.org',
-    license='GPL',
-    packages=['synapse'],
+    license='Apache',
+    packages=['synapseclient'],
     install_requires=[
         'requests>=1.0',
     ],
     entry_points = {
-        'console_scripts': ['synapse = synapse.__main__:main']
+        'console_scripts': ['synapse = synapseclient.__main__:main']
     },
     zip_safe=False,
     classifiers=[
