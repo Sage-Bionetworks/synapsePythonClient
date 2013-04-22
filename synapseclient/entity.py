b78f1987b75b7a9d96e956daf868672e7153d92b
Christopher Bare <chris.bare@sagebase.org>
2013-04-22 15:38:27 -0700

    removed entity2

--- synapseclient/entity.py
+++ synapseclient/entity.py
@@ -50,7 +50,7 @@ class Versionable(object):
 ## Alternate implementations include:
 ##  * a naming convention to tag object members
 ##  * keeping a list of 'transient' variables (the object members)
-##  * giving up on the dot notation
+##  * giving up on the dot notation (implemented in Entity2.py in commit e441fcf5a6963118bcf2b5286c67fc66c004f2b5 in the entity_object branch)
 ##  * giving up on hiding the difference between properties and annotations
 
 class Entity(collections.MutableMapping):
