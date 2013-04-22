ae31e8666ab13eeff77123a990e11f798f5b4e6b
Christopher Bare <chris.bare@sagebase.org>
2013-04-21 19:06:24 -0700

    try an alternate implementation of Entity without changing the behavior of the dot notation

---
+++ synapseclient/entity2.py
@@ -0,0 +1,155 @@
+##
+## Represent a synapse entity
+## chris.bare@sagebase.org
+############################################################
+import collections
+from dict_object import DictObject
+from utils import id_of, entity_type
+import os
+
+
+
+## File, Locationable and Summary are Versionable
+class Versionable(object):
+    synapse_class = 'org.sagebionetworks.repo.model.Versionable'
+    _property_keys = ['versionNumber', 'versionLabel', 'versionComment', 'versionUrl', 'versions']
+
+
+## compromises:
+##   give on dot notation
+##   properties and annotations share a namespace.
+
+
+class Entity(dict):
+    synapse_class = 'org.sagebionetworks.repo.model.Entity'
+    _property_keys = ['id', 'name', 'description', 'parentId',
+                     'entityType', 'concreteType',
+                     'uri', 'etag', 'annotations', 'accessControlList',
+                     'createdOn', 'createdBy', 'modifiedOn', 'modifiedBy']
+
+    @classmethod
+    def create(cls, properties=None, annotations=None):
+        """
+        Create an Entity or a subclass given dictionaries of properties
+        and annotations, as might be received from the Synapse Repository.
+
+        If entityType is defined in properties, we create the proper subclass
+        of Entity. If not, give back the type asked for.
+        """
+        if cls==Entity and 'entityType' in properties and properties['entityType'] in _entity_type_to_class:
+            cls = _entity_type_to_class[properties['entityType']]
+        return cls(properties=properties, annotations=annotations)
+
+    def __init__(self, properties=None, annotations=None, **kwargs):
+        if annotations:
+            if isinstance(annotations, collections.Mapping):
+                for key, value in annotations.items():
+                    self.__setitem__(key, value)
+
+        ## properties will destructively overwrite annotations
+        if properties:
+            if isinstance(properties, collections.Mapping):
+                for key, value in properties.items():
+                    self.__setitem__(key, value)
+
+        for key, value in kwargs.items():
+            self.__setitem__(key, value)
+
+    def properties(self):
+        for key in self._property_keys:
+            if key in self:
+                yield (key, self[key],)
+
+    def annotations(self):
+        for key in self:
+            if key not in self._property_keys:
+                yield (key, self[key],)
+
+
+
+class Project(Entity):
+    _synapse_class = 'org.sagebionetworks.repo.model.Project'
+
+    def __init__(self, name=None, properties=None, annotations=None, **kwargs):
+        if name: kwargs['name'] = name
+        super(Project, self).__init__(entityType=Project._synapse_class, properties=properties, annotations=annotations, **kwargs)
+
+
+class Folder(Entity):
+    _synapse_class = 'org.sagebionetworks.repo.model.Folder'
+
+    def __init__(self, name=None, parent=None, properties=None, annotations=None, **kwargs):
+        if name: kwargs['name'] = name
+        if parent: kwargs['parentId'] = id_of(parent)
+        super(Folder, self).__init__(entityType=Folder._synapse_class, properties=properties, annotations=annotations, **kwargs)
+
+
+class File(Entity, Versionable):
+    _property_keys = Entity._property_keys + Versionable._property_keys + ['dataFileHandleId']
+    _synapse_class = 'org.sagebionetworks.repo.model.FileEntity'
+
+    ## File(path="/path/to/file", synapseStore=True, parentId="syn101")
+    def __init__(self, path=None, parent=None, synapseStore=True, properties=None, annotations=None, **kwargs):
+        if path and 'name' not in kwargs:
+            kwargs['name'] = os.path.basename(path)
+        if parent: kwargs['parentId'] = id_of(parent)
+        super(File, self).__init__(entityType=File._synapse_class, properties=properties, annotations=annotations, **kwargs)
+        if path:
+            self.path = path
+
+
+
+
+
+## Deprecated, but kept around for compatibility with
+## old-style Data, Code, Study, etc. entities
+class Locationable(Versionable):
+    _synapse_class = 'org.sagebionetworks.repo.model.Locationable'
+    _property_keys = Versionable._property_keys + ['locations', 'md5', 'contentType', 's3Token']
+
+
+class Analysis(Entity):
+    _synapse_class = 'org.sagebionetworks.repo.model.Analysis'
+
+
+class Code(Entity, Locationable):
+    _synapse_class = 'org.sagebionetworks.repo.model.Code'
+    _property_keys = Entity._property_keys + Locationable._property_keys
+
+
+class Data(Entity, Locationable):
+    _synapse_class = 'org.sagebionetworks.repo.model.Data'
+    _property_keys = Entity._property_keys + Locationable._property_keys
+
+
+class Study(Entity, Locationable):
+    _synapse_class = 'org.sagebionetworks.repo.model.Study'
+    _property_keys = Entity._property_keys + Locationable._property_keys
+
+
+class Summary(Entity, Versionable):
+    _synapse_class = 'org.sagebionetworks.repo.model.Summary'
+    _property_keys = Entity._property_keys + Versionable._property_keys
+
+
+_entity_type_to_class = {'org.sagebionetworks.repo.model.Project':Project,
+                        'org.sagebionetworks.repo.model.Folder':Folder,
+                        'org.sagebionetworks.repo.model.FileEntity':File}
+
+# import synapseclient
+# import synapseclient.utils as utils
+
+# import uuid
+# unique_name = 'Test Project ' + str(uuid.uuid4())
+
+# project = Project(unique_name, description='A test project')
+# project = syn.store(project)
+
+# filepath = utils.make_bogus_data_file()
+# github_repo = 'https://github.com/Sage-Bionetworks/synapsePythonClient'
+
+# data_file = File(filepath, parent=project, executed=github_repo)
+# data_file = syn.store(data_file)
+
+# data_file = syn.get(data_file.id)
+
---
+++ synapseclient/unit_test_Entity2.py
@@ -0,0 +1,141 @@
+from nose.tools import *
+import collections
+from entity2 import Entity, Project, Folder, File, Data
+
+
+def test_Entity():
+    """Test the basics of creating and accessing properties on an entity"""
+    e =  Entity(name='Test object', description='I hope this works',
+                annotations = dict(foo=123, nerds=['chris','jen','janey'], annotations='How confusing!'),
+                properties  = dict(annotations='/repo/v1/entity/syn1234/annotations',
+                               md5='cdef636522577fc8fb2de4d95875b27c',
+                               parentId='syn1234'),
+                entityType='org.sagebionetworks.repo.model.Data')
+
+    print e
+
+    assert e['parentId'] == 'syn1234'
+    assert e['foo'] == 123
+    assert e['nerds'] == ['chris','jen','janey']
+
+    ## should take the value of the property rather than the annotation
+    assert e['annotations'] == '/repo/v1/entity/syn1234/annotations'
+
+    #print "keys = " + str(e.keys())
+    assert all([ k in e for k in ['name', 'description', 'foo', 'nerds', 'annotations', 'md5', 'parentId']])
+
+    ## test modifying props
+    e['description'] = 'Working, so far'
+    assert e['description'] == 'Working, so far'
+
+    ## test modifying annos
+    e['foo'] = 999
+    assert e['foo'] == 999
+
+    ## test creating a new annotation
+    e['bar'] = 888
+    assert e['bar'] == 888
+
+
+def test_property_generator():
+    project = Project('TestProject', id='syn1001', foo='bar')
+    assert project['name'] == 'TestProject'
+    assert project['foo'] == 'bar'
+
+    for key,value in project.properties():
+        if key=='name':
+            assert value=='TestProject'
+        elif key=='id':
+            assert value=='syn1001'
+        elif key=='foo':
+            assert False
+
+    for key,value in project.annotations():
+        if key=='foo':
+            assert value=='bar'
+        else:
+            assert False
+
+
+
+def test_subclassing():
+    """Test ability to subclass and add a member variable"""
+    
+    ## define a subclass of Entity to make sure subclassing and creating
+    ## a new member variable works
+    class FoobarEntity(Entity):
+        def __init__(self, x, **kwargs):
+            super(FoobarEntity, self).__init__(entityType='org.sagebionetworks.repo.model.Foobar', **kwargs)
+            self.x = x
+
+    foobar = FoobarEntity(123, name='Foobar', foo=123)
+    assert foobar.x == 123
+    assert 'x' in foobar.__dict__
+    assert foobar.__dict__['x'] == 123
+    assert foobar['foo'] == 123
+    assert foobar['name'] == 'Foobar'
+
+
+def test_entity_creation():
+    props = {
+        "id": "syn123456",
+        "entityType": "org.sagebionetworks.repo.model.Folder",
+        "parentId": "syn445566",
+        "name": "Testing123"
+    }
+    annos = {'testing':123}
+    folder = Entity.create(props, annos)
+
+    assert folder['entityType'] == 'org.sagebionetworks.repo.model.Folder'
+    assert folder.__class__ == Folder
+    assert folder['name'] == 'Testing123'
+    assert folder['testing'] == 123
+
+    props = {
+        "id": "syn123456",
+        "entityType": "org.sagebionetworks.repo.model.DoesntExist",
+        "name": "Whatsits"
+    }
+    whatsits = Entity.create(props)
+
+    assert whatsits['entityType'] == 'org.sagebionetworks.repo.model.DoesntExist'
+    assert whatsits.__class__ == Entity
+
+
+def test_entity_constructors():
+    project = Project('TestProject', id='syn1001', foo='bar')
+    assert project['name'] == 'TestProject'
+    assert project['foo'] == 'bar'
+
+    folder = Folder('MyFolder', parent=project, foo='bat', id='syn1002')
+    assert folder['name'] == 'MyFolder'
+    assert folder['foo'] == 'bat'
+    assert folder['parentId'] == 'syn1001'
+
+    a_file = File('/path/to/fabulous_things.zzz', parent=folder, foo='biz')
+    #assert a_file.name == 'fabulous_things.zzz'
+    assert a_file['entityType'] == 'org.sagebionetworks.repo.model.FileEntity'
+    assert a_file['foo'] == 'biz'
+    assert a_file['parentId'] == 'syn1002'
+    assert a_file.path == '/path/to/fabulous_things.zzz'
+
+
+def test_property_keys():
+    assert 'parentId' in File._property_keys
+    assert 'versionNumber' in File._property_keys
+    assert 'dataFileHandleId' in File._property_keys
+
+
+def test_keys_and_iteration():
+    f = File('foo.xyz', parent='syn1234', foo='bar')
+    print f.keys()
+
+    iter_keys = []
+    for key in f:
+        iter_keys.append(key)
+    assert 'parentId' in iter_keys
+    assert 'name' in iter_keys
+    assert 'foo' in iter_keys
+    assert 'entityType' in iter_keys
+
+
