8f969b22d15ceae020ce4c07eba95126797686b8
Christopher Bare <chris.bare@sagebase.org>
2013-10-08 13:27:28 -0700

    fix bug getting Data entities with no associated files

--- tests/unit/unit_tests.py
+++ tests/unit/unit_tests.py
@@ -248,3 +248,19 @@ def test_version_check():
     assert _version_tuple('0.5.1.dev200', levels=3) == ('0', '5', '1')
     assert _version_tuple('1.6', levels=3) == ('1', '6', '0')
 
+def test_normalize_path():
+    ## tests should pass on reasonable OSes and also on windows
+
+    ## resolves relative paths
+    assert len(utils.normalize_path('asdf.txt')) > 8
+
+    ## doesn't resolve home directory references
+    #assert '~' not in utils.normalize_path('~/asdf.txt')
+
+    ## converts back slashes to forward slashes
+    assert utils.normalize_path('\\windows\\why\\why\\why.txt')
+
+    ## what's the right thing to do for None?
+    assert utils.normalize_path(None) is None
+
+
