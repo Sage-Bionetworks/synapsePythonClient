6e37a3f124cdfad7e619b58fa9c0026275a4d319
Christopher Bare <chris.bare@sagebase.org>
2015-05-15 08:30:36 -0700

    debugging jenkins machines - to be deleted

--- tests/integration/integration_test_Entity.py
+++ tests/integration/integration_test_Entity.py
@@ -87,7 +87,7 @@ def test_Entity():
     assert a_file['bday'][0] == Datetime(2013,3,15)
     assert a_file.new_key[0] == 'A newly created value'
     assert a_file.path == path
-    assert a_file.versionNumber == 1
+    assert a_file.versionNumber == 1, "unexpected version number: " +  str(a_file.versionNumber)
 
     # Upload a new File and verify
     new_path = utils.make_bogus_data_file()
