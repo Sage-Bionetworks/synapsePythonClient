68cce4c2f7eecbebde43e2a50ef50baec254ecaa
Christopher Bare <chris.bare@sagebase.org>
2015-09-30 11:20:43 -0700

    fix for gzipped markdown files in getWiki

--- synapseclient/client.py
+++ synapseclient/client.py
@@ -2625,11 +2625,11 @@ class Synapse:
 
             self.cache.add(wiki.markdownFileHandleId, fileInfo['path'])
 
-        if fileInfo['path'].endswith('.gz'):
+        try:
             import gzip
             with gzip.open(fileInfo['path']) as f:
                 markdown = f.read().decode('utf-8')
-        else:
+        except IOError as ex1:
             with open(fileInfo['path']) as f:
                 markdown = f.read().decode('utf-8')
         print markdown
@@ -3097,6 +3097,8 @@ class Synapse:
             if not os.path.exists(downloadLocation):
                 os.makedirs(downloadLocation)
         cached_file_path = self.cache.get(file_handle_id, downloadLocation)
+        ## TODO finish cache refactor by handling collisions and
+        ## TODO copy from cache to downloadLocation
         if cached_file_path is not None:
             return {'path':cached_file_path}
         else:
@@ -3111,6 +3113,22 @@ class Synapse:
             self.cache.add(file_handle_id, file_info['path'])
 
             return file_info
+
+
+    # TODO a function that downloads all files in a table or all
+    # TODO files returned by a query
+    # def downloadTableFiles(table, column, downloadLocation=None, ifcollision="keep.both"):
+    #     """
+    #     :param table:            schema object, table query result or synapse ID
+    #     :param column:           a Column object, the ID of a column or its name
+    #     :param downloadLocation: location in local file system to download the file
+    #     :param ifcollision:      Determines how to handle file collisions.
+    #                              May be "overwrite.local", "keep.local", or "keep.both".
+    #                              Defaults to "keep.both".
+
+    #     :returns: a dictionary with 'path'.
+    #     """
+    #     pass
 
 
     ############################################################
@@ -3194,7 +3212,7 @@ class Synapse:
         """
         send a message via Synapse.
 
-        :param userId: A list of user IDs to which the message is to be sent
+        :param userIds: A list of user IDs to which the message is to be sent
 
         :param messageSubject: The subject for the message
 
