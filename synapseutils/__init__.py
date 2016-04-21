03ef17b25b256afb914703ec34b6286b2bb415da
thomasyu888 <thomasyu888@gmail.com>
2016-04-20 23:19:06 -0700

    Add in synapseutils.walk

--- synapseutils/__init__.py
+++ synapseutils/__init__.py
@@ -4,3 +4,4 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 from .copy import copy, copyWiki
+from .walk import walk
---
+++ synapseutils/walk.py
@@ -0,0 +1,25 @@
+import synapseclient
+from synapseclient.entity import is_container
+import os
+
+def walk(syn,synID,newpath=None):
+    starting = syn.get(synID,downloadFile=False)
+    if newpath is None:
+        dirpath = (starting.name, synID)
+    else:
+        dirpath = (newpath,synID)
+    dirs = []
+    nondirs = []
+    results = syn.chunkedQuery('select id, name, nodeType from entity where parentId == "%s"'%synID)
+    for i in results:
+        if is_container(i):
+            dirs.append((i['entity.name'],i['entity.id']))
+        else:
+            nondirs.append((i['entity.name'],i['entity.id']))
+    yield dirpath, dirs, nondirs
+    for name in dirs:
+        newpath = os.path.join(dirpath[0],name[0])
+        for x in walk(syn, name[1], newpath):
+            yield x
+
+
