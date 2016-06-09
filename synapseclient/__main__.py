7033a3c1dfc4fe39bd40ae877b62ab54b8292cc9
thomasyu888 <thomasyu888@gmail.com>
2016-06-09 16:01:19 -0700

    Add in copy function

--- synapseclient/__main__.py
+++ synapseclient/__main__.py
@@ -43,6 +43,7 @@ Commands
   * **add**              - add or modify content to Synapse
   * **delete**           - removes a dataset from Synapse
   * **mv**               - move a dataset in Synapse
+  * **cp**               - copy an entity/dataset in Synapse
   * **query**            - performs SQL like queries on Synapse
   * **submit**           - submit an entity for evaluation
   * **set-provenance**   - create provenance records
@@ -70,6 +71,7 @@ import collections
 import shutil
 import sys
 import synapseclient
+import synapseutils as synu
 from . import Activity
 from . import utils
 import signal
@@ -228,6 +230,15 @@ def associate(args, syn):
         else:
             print('%s.%i\t%s' %(ent.id, ent.versionNumber, fp))
 
+def copy(args,syn):
+    mappings = synu.copy(syn, args.id, args.destinationId, 
+                         copyWikiPage=args.skipCopyWiki, 
+                         copyTable=args.skipCopyTable, 
+                         mapping=args.mapping, 
+                         version=args.version, replace=args.replace,
+                         setProvenance=args.setProvenance, 
+                         recursive=args.notRecursive)
+    print(mappings)
 
 def cat(args, syn):
     try:
@@ -542,20 +553,30 @@ def build_parser():
             help='Synapse ID of project or folder where file/folder will be moved ')
     parser_mv.set_defaults(func=move)
 
-    # parser_cp = subparsers.add_parser('cp',
-    #         help='Copies a file in Synapse')
-    # parser_cp.add_argument('--id', metavar='syn123', type=str, required=True,
-    #         help='Id of entity in Synapse to be copied.')
-    # parser_cp.add_argument('--parentid', '--parentId', '-parentid', '-parentId', metavar='syn123', type=str, required=True, dest='parentid',
-    #         help='Synapse ID of project or folder where file will be moved ')
-    # parser_cp.add_argument('-v', '--version', metavar='VERSION', type=int, default=None,
-    #         help='Synapse version number of entity to retrieve. Defaults to most recent version.')
-    # parser_cp.add_argument('--setProvenance', metavar='MODE', type=str, default='traceback',
-    #         help=('Has three values to set the provenance of the copied entity-'
-    #                     'traceback: Sets to the source entity'
-    #                     'existing: Sets to source entity\'s original provenance (if it exists)'
-    #                     'None/none: No provenance is set'))
-    # parser_cp.set_defaults(func=copy)
+    parser_cp = subparsers.add_parser('cp',
+            help='Copies a file in Synapse')
+    parser_cp.add_argument('id', metavar='syn123', type=str,
+            help='Id of entity in Synapse to be copied.')
+    parser_cp.add_argument('--destinationId', metavar='syn123', type=str,
+            help='Synapse ID of project or folder where file will be copied to.  If no destinationId specified, a new project is created')
+    parser_cp.add_argument('--version','-v', metavar='1', type=int, default=None,
+            help='Synapse version number of entity to retrieve. Defaults to most recent version.')
+    parser_cp.add_argument('--mapping', metavar='{test:testing}', type=dict, default=dict(),
+            help="Takes a mapping {'oldSynId': 'newSynId'} to help with replacing syn ids in the new wiki pages that aren't part of the entities being copied. Defaults to dict()")
+    parser_cp.add_argument('--setProvenance', metavar='traceback', type=str, default='traceback',
+            help=('Has three values to set the provenance of the copied entity-'
+                        'traceback: Sets to the source entity'
+                        'existing: Sets to source entity\'s original provenance (if it exists)'
+                        'None/none: No provenance is set'))
+    parser_cp.add_argument('-skipCopyTable', action='store_false',
+            help='Skips Table Entities when copying projects')
+    parser_cp.add_argument('-replace', action='store_true',
+            help='Replace File Entities even if there is a file entities that are named the same in the destination')
+    parser_cp.add_argument('-notRecursive', action='store_false',
+            help='Do not copy everything recusively')
+    parser_cp.add_argument('-skipCopyWiki', action='store_false',
+            help='Do not copy the wiki pages')
+    parser_cp.set_defaults(func=copy)
 
     parser_associate = subparsers.add_parser('associate',
             help=('Associate local files with the files stored in Synapse so that calls to '
