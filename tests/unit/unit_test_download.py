7333fbc92788e10ee2c1b1a516b4db3688865e1a
Christopher Bare <christopherbare@gmail.com>
2016-08-23 11:18:26 -0700

    fix simulated partial downloads to return 206 responses

--- tests/unit/unit_test_download.py
+++ tests/unit/unit_test_download.py
@@ -34,11 +34,11 @@ class MockRequestGetFunction(object):
 
 ## a class to iterate bogus content
 class IterateContents(object):
-    def __init__(self, contents, buffer_size, partial=None):
+    def __init__(self, contents, buffer_size, partial_start=0, partial_end=None):
         self.contents = contents
         self.buffer_size = buffer_size
-        self.i = 0
-        self.partial = partial
+        self.i = partial_start
+        self.partial_end = partial_end
 
     def __iter__(self):
         return self
@@ -49,12 +49,12 @@ class IterateContents(object):
     def __next__(self):
         if self.i >= len(self.contents):
             raise StopIteration()
-        if self.partial and self.i >= self.partial:
+        if self.partial_end and self.i >= self.partial_end:
             raise requests.exceptions.ChunkedEncodingError("Simulated partial download! Connection reset by peer!")
         start = self.i
         end   = min(self.i + self.buffer_size, len(self.contents))
-        if self.partial:
-            end = min(end, self.partial)
+        if self.partial_end:
+            end = min(end, self.partial_end)
         self.i = end
         return self.contents[start:end].encode('utf-8')
 
@@ -76,7 +76,7 @@ def create_mock_response(url, response_type, **kwargs):
         response.text = '{"reason":"{}"}'.format(kwargs['reason'])
         response.json = lambda: json.loads(response.text)
     elif response_type=="stream":
-        response.status_code = 200
+        response.status_code = kwargs.get('status_code', 200)
         response.headers = {
             'content-disposition':'attachment; filename="fname.ext"',
             'content-type':'application/octet-stream',
@@ -85,7 +85,8 @@ def create_mock_response(url, response_type, **kwargs):
         response.iter_content = lambda buffer_size: \
             IterateContents(kwargs['contents'],
                             kwargs['buffer_size'],
-                            kwargs.get('partial', None))
+                            kwargs.get('partial_start', 0),
+                            kwargs.get('partial_end', None))
     else:
         response.status_code = 200
         response.text = kwargs['text']
@@ -143,9 +144,9 @@ def test_mock_download():
     ## 3. recover from partial download
     mock_requests_get = MockRequestGetFunction([
         create_mock_response(url, "redirect", location="https://fakeurl.com/asdf"),
-        create_mock_response(url, "stream", contents=contents, buffer_size=1024, partial=len(contents)//7*3),
-        create_mock_response(url, "stream", contents=contents, buffer_size=1024, partial=len(contents)//7*5),
-        create_mock_response(url, "stream", contents=contents, buffer_size=1024)
+        create_mock_response(url, "stream", contents=contents, buffer_size=1024, partial_end=len(contents)//7*3, status_code=200),
+        create_mock_response(url, "stream", contents=contents, buffer_size=1024, partial_start=len(contents)//7*3, partial_end=len(contents)//7*5, status_code=206),
+        create_mock_response(url, "stream", contents=contents, buffer_size=1024, partial_start=len(contents)//7*5, status_code=206)
     ])
 
     ## patch requests.get and also the method that generates signed
@@ -155,14 +156,16 @@ def test_mock_download():
         path = syn._download_with_retries(url, destination=temp_dir, file_handle_id=12345, expected_md5=contents_md5)
 
 
-    ## 4. don't recover
+    ## 4. don't recover, a partial download that never completes
+    ##    should eventually throw an exception
     caught_exception = None
     responses = [
-        create_mock_response(url, "redirect", location="https://fakeurl.com/asdf")
+        create_mock_response(url, "redirect", location="https://fakeurl.com/asdf"),
+        create_mock_response(url, "stream", contents=contents, buffer_size=1024, partial_start=0, partial_end=len(contents)//11, status_code=200)
     ]
     for i in range(1,10):
         responses.append(
-            create_mock_response(url, "stream", contents=contents, buffer_size=1024, partial=len(contents)//11*i))
+            create_mock_response(url, "stream", contents=contents, buffer_size=1024, partial_start=len(contents)//11*i, partial_end=len(contents)//11*(i+1), status_code=206))
     mock_requests_get = MockRequestGetFunction(responses)
 
     ## patch requests.get and also the method that generates signed
@@ -176,4 +179,18 @@ def test_mock_download():
             print("Caught expected exception: ", str(ex))
 
     assert caught_exception, "Expected a SynapseDownloadError"
+
+
+    ## 5. 206 Range header not supported, respond with 200 and full file
+    mock_requests_get = MockRequestGetFunction([
+        create_mock_response(url, "redirect", location="https://fakeurl.com/asdf"),
+        create_mock_response(url, "stream", contents=contents, buffer_size=1024, partial=len(contents)//7*3, status_code=200),
+        create_mock_response(url, "stream", contents=contents, buffer_size=1024, status_code=200)
+    ])
+
+    ## patch requests.get and also the method that generates signed
+    ## headers (to avoid having to be logged in to Synapse)
+    with patch.object(requests, 'get', side_effect=mock_requests_get), \
+         patch.object(synapseclient.client.Synapse, '_generateSignedHeaders', side_effect=mock_generateSignedHeaders):
+        path = syn._download_with_retries(url, destination=temp_dir, file_handle_id=12345, expected_md5=contents_md5)
 
