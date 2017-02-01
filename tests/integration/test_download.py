21d44aef352029c848ea1833e1cd28afb7ab3b9e
Larsson Omberg <lom@larssono.com>
2017-01-31 16:52:03 -0800

    Fix ftp test.

--- tests/integration/test_download.py
+++ tests/integration/test_download.py
@@ -17,7 +17,7 @@ from synapseclient import Activity, Entity, Project, Folder, File
 
 import integration
 from integration import schedule_for_cleanup
-
+import json
 
 
 def setup(module):
@@ -80,20 +80,20 @@ def test_ftp_download():
     
     # Another test with an external reference. This is because we only need to test FTP download; not upload. Also so we don't have to maintain an FTP server just for this purpose.
     # Make an entity that points to an FTP server file.
-	entity = File(parent=project['id'], name = '1KB.zip')
-	fileHandle = {}
-	fileHandle['externalURL'] = 'ftp://speedtest.tele2.net/1KB.zip'
-	fileHandle["fileName"] = entity.name
-	fileHandle["contentType"] = "application/zip"
-	fileHandle["contentMd5"] = '0f343b0931126a20f133d67c2b018a3b'
-	fileHandle["contentSize"] = 1024
-	fileHandle["concreteType"] = "org.sagebionetworks.repo.model.file.ExternalFileHandle"
-	fileHandle = syn.restPOST('/externalFileHandle', json.dumps(fileHandle), syn.fileHandleEndpoint)
-	entity.dataFileHandleId = fileHandle['id']
-	entity = syn.store(entity)
+    entity = File(parent=project['id'], name = '1KB.zip')
+    fileHandle = {}
+    fileHandle['externalURL'] = 'ftp://speedtest.tele2.net/1KB.zip'
+    fileHandle["fileName"] = entity.name
+    fileHandle["contentType"] = "application/zip"
+    fileHandle["contentMd5"] = '0f343b0931126a20f133d67c2b018a3b'
+    fileHandle["contentSize"] = 1024
+    fileHandle["concreteType"] = "org.sagebionetworks.repo.model.file.ExternalFileHandle"
+    fileHandle = syn.restPOST('/externalFileHandle', json.dumps(fileHandle), syn.fileHandleEndpoint)
+    entity.dataFileHandleId = fileHandle['id']
+    entity = syn.store(entity)
 
-	# Download the entity and check that MD5 matches expected
-	FTPfile = syn.get(entity.id, downloadLocation=os.getcwd(), downloadFile=True)
-	assert FTPfile.md5==utils.md5_for_file(FTPfile.path).hexdigest()
-	schedule_for_cleanup(entity)
-	os.remove(FTPfile.path)
+    # Download the entity and check that MD5 matches expected
+    FTPfile = syn.get(entity.id, downloadLocation=os.getcwd(), downloadFile=True)
+    assert FTPfile.md5==utils.md5_for_file(FTPfile.path).hexdigest()
+    schedule_for_cleanup(entity)
+    os.remove(FTPfile.path)
