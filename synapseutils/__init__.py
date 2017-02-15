c221f15fdd1d47f43aec49784af5a9e5f169264e
thomasyu888 <thomasyu888@gmail.com>
2017-02-14 20:44:51 -0800

    Import copy file handle function into synapseutils

--- synapseutils/__init__.py
+++ synapseutils/__init__.py
@@ -19,6 +19,6 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-from .copy import copy, copyWiki
+from .copy import copy, copyWiki, copyFileHandles
 from .walk import walk
 from .sync import syncFromSynapse
