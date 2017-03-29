96512d31e80b4327af90b3da655e4ce1528d3b25
Larsson Omberg <lom@larssono.com>
2017-03-29 13:41:17 -0700

    Adds support for pagination in getWikiHeaders (SYNPY-421)

--- synapseclient/client.py
+++ synapseclient/client.py
@@ -2605,7 +2605,7 @@ class Synapse:
         """
 
         uri = '/entity/%s/wikiheadertree' % id_of(owner)
-        return [DictObject(**header) for header in self.restGET(uri)['results']]
+        return [DictObject(**header) for header in self._GET_paginated(uri)]
 
 
     def _storeWiki(self, wiki):
