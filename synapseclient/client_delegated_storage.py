d22266965ddf73eb85508f468c2a4fbc21e9ec04
unknown <ziming.dong@sagebase.org>
2017-05-24 14:11:19 -0700

    finished implementing download for a client s3

--- synapseclient/client_delegated_storage.py
+++ synapseclient/client_delegated_storage.py
@@ -3,7 +3,6 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 from six import add_metaclass
-from builtins import str
 
 from abc import ABCMeta, abstractmethod
 from synapseclient.utils import itersubclasses
@@ -35,24 +34,29 @@ class ClientS3Connection(ClientDelegatedStorageConnection):
     #TODO: replace with actual concreteType
     _synapse_storageLocation_type = "org.sagebionetworks.placeholder.idk""
 
+    #TODO: perhaps use a @meomoize __new__() instead of init so we dont create the same connection multiple times
+
     def __init__(self,storageLocation, access_key_id = None, secret_access_key = None):
         self._bucket = storageLocation['bucket']
         self._endpoint = storageLocation['endpointUrl'] # TODO: probably don't need to store this
 
-        # TODO: grab config file auth
-        key_id = access_key_id
-        secret_key = secret_access_key
-
-        conn = boto.connect_s3(aws_access_key_id=key_id, aws_secret_access_key=secret_key, host=self.endpoint,
+        #Note: boto will automatically look inside the ~/.aws folder for AWS credentials if aws_access_key_id and aws_secret_access_key are None
+        conn = boto.connect_s3(aws_access_key_id=access_key_id, aws_secret_access_key=secret_access_key, host=self.endpoint,
                         calling_format=boto.s3.connection.OrdinaryCallingFormat())
 
         #TODO: should bucket be created if not exist?
         self._boto_bucket = conn.get_bucket(storageLocation['bucket'])
 
     def downloadFile(self, remotePath, downloadFilePath):
-        #TODO: do filepath existance check if boto does not do this already
-        self._boto_bucket.get_contents_to_filename(remotePath, downloadFilePath)
-        #TODO: error checking?
+        # boto does not check for file existence and will overwrite the file if it already exists
+        if os.path.exists(downloadFilePath):
+            raise ValueError("The file: [%s] already exists", downloadFilePath)
+
+        key = self._boto_bucket.get_key(remotePath)
+        if not key:
+            raise ValueError("The given remote key [%s] does not exist" % remotePath)
+
+        key.get_contents_to_filename(downloadFilePath)
         return downloadFilePath
 
     def uploadFile(self, remotePath, uploadFilePath):
