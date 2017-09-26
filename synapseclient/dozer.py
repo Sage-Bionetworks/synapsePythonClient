3bf7529c940031ee7a1f6dd318c6884b38ead336
bhoff <bruce.hoff@sagebase.org>
2017-09-25 17:40:02 -0700

    SYNR-1125

--- synapseclient/dozer.py
+++ synapseclient/dozer.py
@@ -10,6 +10,8 @@ import time
 _listeners=[]
 
 def add_listener(listener):
+    if not callable(listener):
+        raise ValueError("listener is not callable")
     _listeners.append(listener)
     
 def clear_listeners():
@@ -19,8 +21,6 @@ def doze(secs, listener_check_interval_secs=0.1):
     end_time = time.time()+secs
     while time.time()<end_time:
         for listener in _listeners:
-            if not callable(listener):
-                raise ValueError("listener is not callable")
             listener()
         time.sleep(listener_check_interval_secs)
         
--- tests/unit/unit_test_doze.py
+++ tests/unit/unit_test_doze.py
@@ -7,19 +7,21 @@ import synapseclient.dozer as doze
 
 def teardown():
     doze.clear_listeners()
-    
-counter=0
+
    
 def test_doze():
-    global counter
-    counter=0
     
-    def counter_inc():
-        global counter
-        counter = counter+1
+    class CounterClass(object):
+        def __init__(self):
+            self.val = 0
+            
+        def __call__(self):
+            self.val = self.val + 1
+    
+    counter = CounterClass()
     
     # register Listener
-    doze.add_listener(counter_inc)
+    doze.add_listener(counter)
     doze.doze(1) # should call counter_inc() about 10 times
-    assert counter > 0
+    assert counter.val > 0
     
