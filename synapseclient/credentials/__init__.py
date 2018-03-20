e86ece3c35c38975ef5a6385132c845e993c64a9
Ziming Dong <ziming.dong@sagebase.org>
2018-03-19 18:25:43 -0700

    fixed imports

--- synapseclient/credentials/__init__.py
+++ synapseclient/credentials/__init__.py
@@ -1,3 +1,3 @@
-from credential_provider import get_default_credential_chain
-from cred_data import UserLoginArgs
-import cached_sessions
+from .credential_provider import get_default_credential_chain
+from .cred_data import UserLoginArgs
+from . import cached_sessions
