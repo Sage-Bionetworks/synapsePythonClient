645de86fc8ad3f46fff2ed07e7223d33791d46f2
Kimyen <kimyen0816@gmail.com>
2018-06-07 16:54:39 -0700

    use stderr for warnings

--- synapseclient/__main__.py
+++ synapseclient/__main__.py
@@ -99,7 +99,7 @@ def query(args, syn):
             sys.stdout.write("%s\n" % ("\t".join(row)))
     else:
         # chunkedQuery will be removed in 1.9.0
-        sys.stdout.write('This query is deprecated. Query should only be used for table and view.'
+        sys.stderr.write('This query is deprecated. Query should only be used for table and view.'
                          ' Please consider using the ls function.')
         results = syn.chunkedQuery(' '.join(args.queryString))
         headings = collections.OrderedDict()
