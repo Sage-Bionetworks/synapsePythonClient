c70c59a9991cc02139cbadc439f7acc3ac8d07b1
Kimyen <kimyen0816@gmail.com>
2018-07-27 16:09:28 -0700

    test keyring version 13.2.1

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(name='synapseclient',
         'six',
         'future',
         'backports.csv',
-        'keyring==11.0.0',
+        'keyring==13.2.1',
         'deprecation',
     ],
     extras_require = {
