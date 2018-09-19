d6d28e2db45665c924b396daac1e8d61ee7b804a
Kimyen <kimyen0816@gmail.com>
2018-09-19 10:49:34 -0700

    fix the version we are supporting to exclude 3.7+

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ import sys
 from os.path import expanduser, exists
 
 # check Python version, before we do anything
-if sys.version_info < (2, 7, 0):
-    sys.stderr.write("The Synapse Client for Python requires Python 2.7 or 3.4 or higher.\n")
+if sys.version_info[:2] not in [(2, 7), (3, 5), (3, 6)]:
+    sys.stderr.write("The Synapse Client for Python requires Python 2.7 or 3.5 or 3.6.\n")
     sys.stderr.write("Your Python appears to be version %d.%d.%d\n" % sys.version_info[:3])
     sys.exit(-1)
 
