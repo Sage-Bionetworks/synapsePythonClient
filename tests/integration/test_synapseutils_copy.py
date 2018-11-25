63a25791cbefe37559cc225b763ba54cfb991ce3
Thomas Yu <thomasyu888@gmail.com>
2018-11-24 16:53:20 -0800

    Refactor copy integration tests

--- tests/integration/test_synapseutils_copy.py
+++ tests/integration/test_synapseutils_copy.py
@@ -24,179 +24,179 @@ def setup(module):
 
 # Add Test for UPDATE
 # Add test for existing provenance but the orig doesn't have provenance
-def test_copy():
-    """Tests the copy function"""
-    # Create a Project
-    project_entity = syn.store(Project(name=str(uuid.uuid4())))
-    schedule_for_cleanup(project_entity.id)
-    # Create two Folders in Project
-    folder_entity = syn.store(Folder(name=str(uuid.uuid4()), parent=project_entity))
-    second_folder = syn.store(Folder(name=str(uuid.uuid4()), parent=project_entity))
-    third_folder = syn.store(Folder(name=str(uuid.uuid4()), parent=project_entity))
-    schedule_for_cleanup(folder_entity.id)
-    schedule_for_cleanup(second_folder.id)
-    schedule_for_cleanup(third_folder.id)
+class TestCopy:
+    
+    def setup(self):
+        # Create a Project
+        self.project_entity = syn.store(Project(name=str(uuid.uuid4())))
+        schedule_for_cleanup(self.project_entity.id)
+        # Create three Folders in Project
+        self.folder_entity = syn.store(Folder(name=str(uuid.uuid4()), parent=self.project_entity))
+        self.second_folder = syn.store(Folder(name=str(uuid.uuid4()), parent=self.project_entity))
+        self.third_folder = syn.store(Folder(name=str(uuid.uuid4()), parent=self.project_entity))
+        schedule_for_cleanup(self.folder_entity.id)
+        schedule_for_cleanup(self.second_folder.id)
+        schedule_for_cleanup(self.third_folder.id)
 
-    # Annotations and provenance
-    repo_url = 'https://github.com/Sage-Bionetworks/synapsePythonClient'
-    annos = {'test': ['hello_world']}
-    prov = Activity(name="test", used=repo_url)
-    # Create, upload, and set annotations/provenance on a file in Folder
-    filename = utils.make_bogus_data_file()
-    schedule_for_cleanup(filename)
-    file_entity = syn.store(File(filename, parent=folder_entity))
-    externalURL_entity = syn.store(File(repo_url, name='rand', parent=folder_entity, synapseStore=False))
-    syn.setAnnotations(file_entity, annos)
-    syn.setAnnotations(externalURL_entity, annos)
-    syn.setProvenance(externalURL_entity.id, prov)
-    schedule_for_cleanup(file_entity.id)
-    schedule_for_cleanup(externalURL_entity.id)
-    # ------------------------------------
-    # TEST COPY FILE
-    # ------------------------------------
-    output = synapseutils.copy(syn, file_entity.id, destinationId=project_entity.id)
-    output_URL = synapseutils.copy(syn, externalURL_entity.id, destinationId=project_entity.id,
-                                   skipCopyAnnotations=True)
+        # Annotations and provenance
+        self.repo_url = 'https://github.com/Sage-Bionetworks/synapsePythonClient'
+        self.annos = {'test': ['hello_world']}
+        self.prov = Activity(name="test", used=self.repo_url)
+        # Create, upload, and set annotations/provenance on a file in Folder
+        filename = utils.make_bogus_data_file()
+        schedule_for_cleanup(filename)
+        self.file_entity = syn.store(File(filename, parent=self.folder_entity))
+        self.url_entity_name = 'rand'
+        self.externalURL_entity = syn.store(File(self.repo_url, name=self.url_entity_name, parent=self.folder_entity, synapseStore=False))
+        syn.setAnnotations(self.file_entity, self.annos)
+        syn.setAnnotations(self.externalURL_entity, self.annos)
+        syn.setProvenance(self.externalURL_entity.id, self.prov)
+        schedule_for_cleanup(self.file_entity.id)
+        schedule_for_cleanup(self.externalURL_entity.id)
 
-    # Verify that our copied files are identical
-    copied_ent = syn.get(output[file_entity.id])
-    copied_URL_ent = syn.get(output_URL[externalURL_entity.id], downloadFile=False)
+        #Create Link entity
+        second_file = utils.make_bogus_data_file()
+        schedule_for_cleanup(second_file)
+        second_file_entity = syn.store(File(second_file, parent=self.project_entity))
+        self.link_entity = syn.store(Link(second_file_entity.id, parent=self.folder_entity.id))
+        schedule_for_cleanup(second_file_entity.id)
+        schedule_for_cleanup(self.link_entity.id)
 
-    copied_ent_annot = syn.getAnnotations(copied_ent)
-    copied_url_annot = syn.getAnnotations(copied_URL_ent)
-    copied_prov = syn.getProvenance(copied_ent)
-    copied_url_prov = syn.getProvenance(copied_URL_ent)
-    schedule_for_cleanup(copied_ent.id)
-    schedule_for_cleanup(copied_URL_ent.id)
 
-    # TEST: set_Provenance = Traceback
-    assert_equals(copied_prov['used'][0]['reference']['targetId'], file_entity.id)
-    assert_equals(copied_url_prov['used'][0]['reference']['targetId'], externalURL_entity.id)
+        #Create Tables and second project
+        self.second_project = syn.store(Project(name=str(uuid.uuid4())))
+        schedule_for_cleanup(self.second_project.id)
+        cols = [Column(name='n', columnType='DOUBLE', maximumSize=50),
+                Column(name='c', columnType='STRING', maximumSize=50),
+                Column(name='i', columnType='INTEGER')]
+        self.data = [[2.1, 'foo', 10],
+                [2.2, 'bar', 20],
+                [2.3, 'baz', 30]]
 
-    # TEST: Make sure copied files are the same
-    assert_equals(copied_ent_annot, annos)
-    assert_equals(copied_ent.dataFileHandleId, file_entity.dataFileHandleId)
+        self.schema = syn.store(Schema(name='Testing', columns=cols, parent=self.project_entity.id))
+        syn.store(RowSet(schema=self.schema, rows=[Row(r) for r in self.data]))
 
-    # TEST: Make sure copied URLs are the same
-    assert_equals(copied_url_annot, {})
-    assert_equals(copied_URL_ent.externalURL, repo_url)
-    assert_equals(copied_URL_ent.name, 'rand')
-    assert_equals(copied_URL_ent.dataFileHandleId, externalURL_entity.dataFileHandleId)
+        schedule_for_cleanup(self.schema.id)
 
-    # TEST: Throw error if file is copied to a folder/project that has a file with the same filename
-    assert_raises(ValueError, synapseutils.copy, syn, project_entity.id, destinationId=project_entity.id)
-    assert_raises(ValueError, synapseutils.copy, syn, file_entity.id, destinationId=project_entity.id)
-    assert_raises(ValueError, synapseutils.copy, syn, file_entity.id, destinationId=third_folder.id,
-                  setProvenance="gib")
-    assert_raises(ValueError, synapseutils.copy, syn, file_entity.id, destinationId=file_entity.id)
+        self.third_project = syn.store(Project(name=str(uuid.uuid4())))
+        schedule_for_cleanup(self.third_project.id)
 
-    # Test: setProvenance = None
-    output = synapseutils.copy(syn, file_entity.id, destinationId=second_folder.id, setProvenance=None)
-    assert_raises(SynapseHTTPError, syn.getProvenance, output[file_entity.id])
-    schedule_for_cleanup(output[file_entity.id])
+    def test_copy_file(self):
+        """Test Copying Files"""
 
-    # Test: setProvenance = Existing
-    output_URL = synapseutils.copy(syn, externalURL_entity.id, destinationId=second_folder.id, setProvenance="existing")
-    output_prov = syn.getProvenance(output_URL[externalURL_entity.id])
-    schedule_for_cleanup(output_URL[externalURL_entity.id])
-    assert_equals(output_prov['name'], prov['name'])
-    assert_equals(output_prov['used'], prov['used'])
+        output = synapseutils.copy(syn, self.file_entity.id, destinationId=self.project_entity.id)
+        output_URL = synapseutils.copy(syn, self.externalURL_entity.id, destinationId=self.project_entity.id,
+                                       skipCopyAnnotations=True)
 
-    # ------------------------------------
-    # TEST COPY LINKS
-    # ------------------------------------
-    second_file = utils.make_bogus_data_file()
-    # schedule_for_cleanup(filename)
-    second_file_entity = syn.store(File(second_file, parent=project_entity))
-    link_entity = Link(second_file_entity.id, parent=folder_entity.id)
-    link_entity = syn.store(link_entity)
+        # Verify that our copied files are identical
+        copied_ent = syn.get(output[self.file_entity.id])
+        copied_URL_ent = syn.get(output_URL[self.externalURL_entity.id], downloadFile=False)
+        copied_ent_annot = syn.getAnnotations(copied_ent)
+        copied_url_annot = syn.getAnnotations(copied_URL_ent)
+        copied_prov = syn.getProvenance(copied_ent)
+        copied_url_prov = syn.getProvenance(copied_URL_ent)
+        schedule_for_cleanup(copied_ent.id)
+        schedule_for_cleanup(copied_URL_ent.id)
 
-    copied_link = synapseutils.copy(syn, link_entity.id, destinationId=second_folder.id)
-    old = syn.get(link_entity.id, followLink=False)
-    new = syn.get(copied_link[link_entity.id], followLink=False)
-    assert_equals(old.linksTo['targetId'], new.linksTo['targetId'])
+        # TEST: set_Provenance = Traceback
+        assert_equals(copied_prov['used'][0]['reference']['targetId'], self.file_entity.id)
+        assert_equals(copied_url_prov['used'][0]['reference']['targetId'], self.externalURL_entity.id)
 
-    schedule_for_cleanup(second_file_entity.id)
-    schedule_for_cleanup(link_entity.id)
-    schedule_for_cleanup(copied_link[link_entity.id])
+        # TEST: Make sure copied files are the same
+        assert_equals(copied_ent_annot, self.annos)
+        assert_equals(copied_ent.dataFileHandleId, self.file_entity.dataFileHandleId)
 
-    time.sleep(3)
+        # TEST: Make sure copied URLs are the same
+        assert_equals(copied_url_annot, {})
+        assert_equals(copied_URL_ent.externalURL, self.repo_url)
+        assert_equals(copied_URL_ent.name, self.url_entity_name)
+        assert_equals(copied_URL_ent.dataFileHandleId, self.externalURL_entity.dataFileHandleId)
 
-    assert_raises(ValueError, synapseutils.copy, syn, link_entity.id, destinationId=second_folder.id)
+        # TEST: Throw error if file is copied to a folder/project that has a file with the same filename
+        assert_raises(ValueError, synapseutils.copy, syn, self.project_entity.id, destinationId=self.project_entity.id)
+        assert_raises(ValueError, synapseutils.copy, syn, self.file_entity.id, destinationId=self.project_entity.id)
+        assert_raises(ValueError, synapseutils.copy, syn, self.file_entity.id, destinationId=self.third_folder.id,
+                      setProvenance="gib")
+        assert_raises(ValueError, synapseutils.copy, syn, self.file_entity.id, destinationId=self.file_entity.id)
 
-    # ------------------------------------
-    # TEST COPY TABLE
-    # ------------------------------------
-    second_project = syn.store(Project(name=str(uuid.uuid4())))
-    schedule_for_cleanup(second_project.id)
-    cols = [Column(name='n', columnType='DOUBLE', maximumSize=50),
-            Column(name='c', columnType='STRING', maximumSize=50),
-            Column(name='i', columnType='INTEGER')]
-    data = [[2.1, 'foo', 10],
-            [2.2, 'bar', 20],
-            [2.3, 'baz', 30]]
+        # Test: setProvenance = None
+        output = synapseutils.copy(syn, self.file_entity.id, destinationId=self.second_folder.id, setProvenance=None)
+        assert_raises(SynapseHTTPError, syn.getProvenance, output[self.file_entity.id])
+        schedule_for_cleanup(output[self.file_entity.id])
 
-    schema = syn.store(Schema(name='Testing', columns=cols, parent=project_entity.id))
-    syn.store(RowSet(schema=schema, rows=[Row(r) for r in data]))
+        # Test: setProvenance = Existing
+        output_URL = synapseutils.copy(syn, self.externalURL_entity.id, destinationId=self.second_folder.id, setProvenance="existing")
+        output_prov = syn.getProvenance(output_URL[self.externalURL_entity.id])
+        schedule_for_cleanup(output_URL[self.externalURL_entity.id])
+        assert_equals(output_prov['name'], self.prov['name'])
+        assert_equals(output_prov['used'], self.prov['used'])
 
-    table_map = synapseutils.copy(syn, schema.id, destinationId=second_project.id)
-    copied_table = syn.tableQuery('select * from %s' % table_map[schema.id])
-    rows = copied_table.asRowSet()['rows']
-    # TEST: Check if all values are the same
-    for i, row in enumerate(rows):
-        assert_equals(row['values'], data[i])
+    def test_copy_links(self):
+        '''Test Copy Links'''
+        copied_link = synapseutils.copy(syn, self.link_entity.id, destinationId=self.second_folder.id)
+        original_link = syn.get(self.link_entity.id, followLink=False)
+        new_link = syn.get(copied_link[self.link_entity.id], followLink=False)
+        assert_equals(original_link.linksTo['targetId'], new_link.linksTo['targetId'])
 
-    assert_raises(ValueError, synapseutils.copy, syn, schema.id, destinationId=second_project.id)
+        schedule_for_cleanup(copied_link[self.link_entity.id])
 
-    schedule_for_cleanup(schema.id)
-    schedule_for_cleanup(table_map[schema.id])
+        time.sleep(3)
 
-    # ------------------------------------
-    # TEST COPY FOLDER
-    # ------------------------------------
-    mapping = synapseutils.copy(syn, folder_entity.id, destinationId=second_project.id)
-    for i in mapping:
-        old = syn.get(i, downloadFile=False)
-        new = syn.get(mapping[i], downloadFile=False)
-        assert_equals(old.name, new.name)
-        assert_equals(old.annotations, new.annotations)
-        assert_equals(old.concreteType, new.concreteType)
+        assert_raises(ValueError, synapseutils.copy, syn, self.link_entity.id, destinationId=self.second_folder.id)
 
-    assert_raises(ValueError, synapseutils.copy, syn, folder_entity.id, destinationId=second_project.id)
-    # TEST: Throw error if excludeTypes isn't in file, link and table or isn't a list
-    assert_raises(ValueError, synapseutils.copy, syn, second_folder.id, destinationId=second_project.id,
-                  excludeTypes=["foo"])
-    assert_raises(ValueError, synapseutils.copy, syn, second_folder.id, destinationId=second_project.id,
-                  excludeTypes="file")
-    # TEST: excludeType = ["file"], only the folder is created
-    second = synapseutils.copy(syn, second_folder.id, destinationId=second_project.id,
-                               excludeTypes=["file", "table", "link"])
+    def test_copy_table(self):
+        '''Test Copy Table'''
+        table_map = synapseutils.copy(syn, self.schema.id, destinationId=self.second_project.id)
+        schedule_for_cleanup(table_map[self.schema.id])
 
-    copied_folder = syn.get(second[second_folder.id])
-    assert_equals(copied_folder.name, second_folder.name)
-    assert_equals(len(second), 1)
-    # TEST: Make sure error is thrown if foldername already exists
+        copied_table = syn.tableQuery('select * from %s' % table_map[self.schema.id])
+        rows = copied_table.asRowSet()['rows']
+        # TEST: Check if all values are the same
+        for i, row in enumerate(rows):
+            assert_equals(row['values'], self.data[i])
 
-    assert_raises(ValueError, synapseutils.copy, syn, second_folder.id, destinationId=second_project.id)
+        assert_raises(ValueError, synapseutils.copy, syn, self.schema.id, destinationId=self.second_project.id)
 
-    # ------------------------------------
-    # TEST COPY PROJECT
-    # ------------------------------------
-    third_project = syn.store(Project(name=str(uuid.uuid4())))
-    schedule_for_cleanup(third_project.id)
-
-    mapping = synapseutils.copy(syn, project_entity.id, destinationId=third_project.id)
-    for i in mapping:
-        old = syn.get(i, downloadFile=False)
-        new = syn.get(mapping[i], downloadFile=False)
-        if not isinstance(old, Project):
+    def test_copy_folder(self):
+        '''Test Copy Folder'''
+        mapping = synapseutils.copy(syn, self.folder_entity.id, destinationId=self.second_project.id)
+        for i in mapping:
+            old = syn.get(i, downloadFile=False)
+            new = syn.get(mapping[i], downloadFile=False)
+            schedule_for_cleanup(new.id)
             assert_equals(old.name, new.name)
-        assert_equals(old.annotations, new.annotations)
-        assert_equals(old.concreteType, new.concreteType)
+            assert_equals(old.annotations, new.annotations)
+            assert_equals(old.concreteType, new.concreteType)
 
-    # TEST: Can't copy project to a folder
-    assert_raises(ValueError, synapseutils.copy, syn, project_entity.id, destinationId=second_folder.id)
+        assert_raises(ValueError, synapseutils.copy, syn, self.folder_entity.id, destinationId=self.second_project.id)
+        # TEST: Throw error if excludeTypes isn't in file, link and table or isn't a list
+        assert_raises(ValueError, synapseutils.copy, syn, self.second_folder.id, destinationId=self.second_project.id,
+                      excludeTypes=["foo"])
+        assert_raises(ValueError, synapseutils.copy, syn, self.second_folder.id, destinationId=self.second_project.id,
+                      excludeTypes="file")
+        # TEST: excludeType = ["file"], only the folder is created
+        second = synapseutils.copy(syn, self.second_folder.id, destinationId=self.second_project.id,
+                                   excludeTypes=["file", "table", "link"])
 
+        copied_folder = syn.get(second[self.second_folder.id])
+        assert_equals(copied_folder.name, self.second_folder.name)
+        assert_equals(len(second), 1)
+        # TEST: Make sure error is thrown if foldername already exists
+        assert_raises(ValueError, synapseutils.copy, syn, self.second_folder.id, destinationId=self.second_project.id)
+
+    def test_copy_project(self):
+        '''Test copy project'''
+        mapping = synapseutils.copy(syn, self.project_entity.id, destinationId=self.third_project.id)
+        for i in mapping:
+            old = syn.get(i, downloadFile=False)
+            new = syn.get(mapping[i], downloadFile=False)
+            if not isinstance(old, Project):
+                assert_equals(old.name, new.name)
+            assert_equals(old.annotations, new.annotations)
+            assert_equals(old.concreteType, new.concreteType)
+
+        # TEST: Can't copy project to a folder
+        assert_raises(ValueError, synapseutils.copy, syn, self.project_entity.id, destinationId=self.second_folder.id)
 
 class TestCopyWiki:
 
