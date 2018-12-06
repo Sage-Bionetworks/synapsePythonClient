f80f02f77bd81564044fb0701ade1ac11903d15d
thomasyu888 <thomasyu888@gmail.com>
2018-12-06 01:38:03 -0800

    Start adding in unit tests

--- tests/unit/unit_test_synapseutils_copy.py
+++ tests/unit/unit_test_synapseutils_copy.py
@@ -5,14 +5,45 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 import unit
-from nose.tools import assert_raises
+from nose.tools import assert_raises, assert_equals
 from mock import patch, call
 
 import synapseutils
+from synapseclient import Activity, Wiki, Project, Folder, File, Link, Column, Schema, RowSet, Row, EntityViewSchema
 
+import uuid
 
 def setup(module):
     module.syn = unit.syn
+
+class test_copy:
+    
+    def setup(self):
+        project_entity = Project(name=str(uuid.uuid4()))
+        second_project = Project(name=str(uuid.uuid4()))
+        with patch.object(syn, "store", return_value=project_entity) as first_project, \
+             patch.object(syn, "store", return_value=second_project) as second_project:
+            self.project_entity = syn.store(first_project)
+            self.project_entity.id = "syn1234"
+            self.second_project = syn.store(second_project)
+            self.second_project.id = "syn2345"
+
+    def test_copy_EntityView(self):
+        cols = [Column(name='n', columnType='DOUBLE', maximumSize=50),
+                Column(name='c', columnType='STRING', maximumSize=50),
+                Column(name='i', columnType='INTEGER')]
+
+        entity_view_schema = EntityViewSchema(name='TestingEntityView', columns=cols, parent=self.project_entity.id, scopes=[self.project_entity.id])
+        entity_view_schema.id = "syn3456"
+        copied_view_schema = EntityViewSchema(name='TestingEntityView', columns=cols, parent=self.second_project.id, scopes=[self.project_entity.id])
+        copied_view_schema.id = "syn1111"
+
+        with patch.object(syn, "get", return_value=entity_view_schema) as entity_view_schema, \
+             patch.object(syn, "findEntityId", return_value=None), \
+             patch.object(syn, "store", return_value=copied_view_schema):
+            entity_view_schema_map = synapseutils.copy(syn, entity_view_schema.id, destinationId=self.second_project.id, skipCopyWikiPage=True)
+
+        assert_equals(entity_view_schema_map, {"syn3456":"syn1111"})
 
 
 def test_copyWiki_empty_Wiki():
