be52622c6ad1401152e73b4cf3b0a43b70a3d8e6
thomasyu888 <thomasyu888@gmail.com>
2018-12-08 16:15:36 -0800

    Add in unit tests for copying permissions

--- tests/unit/unit_test_synapseutils_copy.py
+++ tests/unit/unit_test_synapseutils_copy.py
@@ -5,14 +5,61 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 import unit
-from nose.tools import assert_raises
+from nose.tools import assert_raises, assert_equals
 from mock import patch, call
+from synapseclient import Project, File, Schema
 
+import uuid
 import synapseutils
 
 
 def setup(module):
     module.syn = unit.syn
+
+class test_copy:
+    
+    def setup(self):
+        self.project_entity = Project(name=str(uuid.uuid4()))
+        self.second_project = Project(name=str(uuid.uuid4()))
+        self.project_entity.id = "syn1234"
+        self.second_project.id = "syn2345"
+
+    def test_copy_permissions(self):
+
+        file_ent = File(name='File', parent=self.project_entity.id)
+        file_ent.id = "syn3456"
+        schema_ent = Schema(name='Testing', parent=self.project_entity.id)
+        schema_ent.id = "syn2456"
+
+        user_name = {'userName':'test'}
+        permissions = ["READ"]
+        access_requirements = {'totalNumberOfResults':0}
+
+        #TEST: File entity READ permissions not copied
+        with patch.object(syn, "get", return_value=file_ent) as patch_syn_get, \
+             patch.object(syn, "getUserProfile", return_value=user_name) as patch_syn_get_profile, \
+             patch.object(syn, "getPermissions", return_value=permissions) as patch_syn_permissions, \
+             patch.object(syn, "restGET", return_value=access_requirements) as patch_syn_restget:
+            copied_file = synapseutils.copy(syn, patch_syn_get.id, destinationId=self.second_project.id, skipCopyWikiPage=True)
+            assert_equals(copied_file, dict())
+
+        #TEST: Schema entity READ permissions not copied
+        with patch.object(syn, "get", return_value=schema_ent) as patch_syn_get, \
+             patch.object(syn, "getUserProfile", return_value=user_name) as patch_syn_get_profile, \
+             patch.object(syn, "getPermissions", return_value=permissions) as patch_syn_permissions, \
+             patch.object(syn, "restGET", return_value=access_requirements) as patch_syn_restget:
+            copied_file = synapseutils.copy(syn, patch_syn_get.id, destinationId=self.second_project.id, skipCopyWikiPage=True)
+            assert_equals(copied_file, dict())
+
+        permissions = ["READ","DOWNLOAD"]
+        access_requirements = {'totalNumberOfResults':1}
+        #TEST: Entity access requirement > 0 not copied
+        with patch.object(syn, "get", return_value=file_ent) as patch_syn_get, \
+             patch.object(syn, "getUserProfile", return_value=user_name) as patch_syn_get_profile, \
+             patch.object(syn, "getPermissions", return_value=permissions) as patch_syn_permissions, \
+             patch.object(syn, "restGET", return_value=access_requirements) as patch_syn_restget:
+            copied_file = synapseutils.copy(syn, patch_syn_get.id, destinationId=self.second_project.id, skipCopyWikiPage=True)
+            assert_equals(copied_file, dict())
 
 
 def test_copyWiki_empty_Wiki():
