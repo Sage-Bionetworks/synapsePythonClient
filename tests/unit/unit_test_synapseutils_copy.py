9e911bf3f7a5c42aaf2a1c1de002cf2ab484ceab
thomasyu888 <thomasyu888@gmail.com>
2018-12-11 00:14:19 -0800

    Split up unit tests

--- tests/unit/unit_test_synapseutils_copy.py
+++ tests/unit/unit_test_synapseutils_copy.py
@@ -16,46 +16,48 @@ import synapseutils
 def setup(module):
     module.syn = unit.syn
 
-class test_copy:
+class test_copy_permissions:
     
     def setup(self):
         self.project_entity = Project(name=str(uuid.uuid4()))
         self.second_project = Project(name=str(uuid.uuid4()))
         self.project_entity.id = "syn1234"
         self.second_project.id = "syn2345"
+        self.file_ent = File(name='File', parent=self.project_entity.id)
+        self.file_ent.id = "syn3456"
+        self.schema_ent = Schema(name='Testing', parent=self.project_entity.id)
+        self.schema_ent.id = "syn2456"
+        self.user_name = {'userName':'test'}
 
-    def test_copy_permissions(self):
-
-        file_ent = File(name='File', parent=self.project_entity.id)
-        file_ent.id = "syn3456"
-        schema_ent = Schema(name='Testing', parent=self.project_entity.id)
-        schema_ent.id = "syn2456"
-
-        user_name = {'userName':'test'}
+    def test_copy_file_read_permissions(self):
+        #TEST: File entity READ permissions not copied
         permissions = ["READ"]
         access_requirements = {'totalNumberOfResults':0}
 
-        #TEST: File entity READ permissions not copied
-        with patch.object(syn, "get", return_value=file_ent) as patch_syn_get, \
-             patch.object(syn, "getUserProfile", return_value=user_name) as patch_syn_get_profile, \
+        with patch.object(syn, "get", return_value=self.file_ent) as patch_syn_get, \
+             patch.object(syn, "getUserProfile", return_value=self.user_name) as patch_syn_get_profile, \
              patch.object(syn, "getPermissions", return_value=permissions) as patch_syn_permissions, \
              patch.object(syn, "restGET", return_value=access_requirements) as patch_syn_restget:
             copied_file = synapseutils.copy(syn, patch_syn_get.id, destinationId=self.second_project.id, skipCopyWikiPage=True)
             assert_equals(copied_file, dict())
 
+    def test_copy_schema_read_permissions(self):
         #TEST: Schema entity READ permissions not copied
-        with patch.object(syn, "get", return_value=schema_ent) as patch_syn_get, \
-             patch.object(syn, "getUserProfile", return_value=user_name) as patch_syn_get_profile, \
+        permissions = ["READ"]
+        access_requirements = {'totalNumberOfResults':0}
+        with patch.object(syn, "get", return_value=self.schema_ent) as patch_syn_get, \
+             patch.object(syn, "getUserProfile", return_value=self.user_name) as patch_syn_get_profile, \
              patch.object(syn, "getPermissions", return_value=permissions) as patch_syn_permissions, \
              patch.object(syn, "restGET", return_value=access_requirements) as patch_syn_restget:
             copied_file = synapseutils.copy(syn, patch_syn_get.id, destinationId=self.second_project.id, skipCopyWikiPage=True)
             assert_equals(copied_file, dict())
 
+    def test_copy_entity_access_requirements(self):
+        #TEST: Entity access requirement > 0 not copied
         permissions = ["READ","DOWNLOAD"]
         access_requirements = {'totalNumberOfResults':1}
-        #TEST: Entity access requirement > 0 not copied
-        with patch.object(syn, "get", return_value=file_ent) as patch_syn_get, \
-             patch.object(syn, "getUserProfile", return_value=user_name) as patch_syn_get_profile, \
+        with patch.object(syn, "get", return_value=self.file_ent) as patch_syn_get, \
+             patch.object(syn, "getUserProfile", return_value=self.user_name) as patch_syn_get_profile, \
              patch.object(syn, "getPermissions", return_value=permissions) as patch_syn_permissions, \
              patch.object(syn, "restGET", return_value=access_requirements) as patch_syn_restget:
             copied_file = synapseutils.copy(syn, patch_syn_get.id, destinationId=self.second_project.id, skipCopyWikiPage=True)
