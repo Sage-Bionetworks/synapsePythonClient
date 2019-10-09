cee2e557e655d1285d3a98c54b2b74244199b8b3
Thomas Yu <thomasyu888@gmail.com>
2019-10-09 11:03:14 -0700

    Make sure entities with access restrictions aren't copied

--- tests/unit/unit_test_synapseutils_copy.py
+++ tests/unit/unit_test_synapseutils_copy.py
@@ -4,10 +4,12 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-import unit
-from nose.tools import assert_raises
 from mock import patch, call
+from nose.tools import assert_raises, assert_equals
+import unit
+import uuid
 
+from synapseclient import Project, File
 import synapseutils
 
 
@@ -48,3 +50,27 @@ def test_copyWiki_input_validation():
 
         assert_raises(ValueError, synapseutils.copyWiki, syn, "syn123", "syn456", entitySubPageId="some_string",
                               updateLinks=False)
+
+
+class TestCopyAccessRestriction:
+    """Test that entities with access restrictions aren't copied"""
+    def setup(self):
+        self.project_entity = Project(name=str(uuid.uuid4()), id="syn1234")
+        self.second_project = Project(name=str(uuid.uuid4()), id="syn2345")
+        self.file_ent = File(name='File', parent=self.project_entity.id)
+        self.file_ent.id = "syn3456"
+
+    def test_copy_entity_access_requirements(self):
+        #TEST: Entity access requirement > 0 not copied
+        access_requirements = {'results': ["fee", "fi"]}
+        with patch.object(syn, "get",
+                          return_value=self.file_ent) as patch_syn_get, \
+             patch.object(syn, "restGET",
+                          return_value=access_requirements) as patch_restget:
+            copied_file = synapseutils.copy(syn, self.file_ent,
+                                            destinationId=self.second_project.id,
+                                            skipCopyWikiPage=True)
+            assert_equals(copied_file, dict())
+            patch_syn_get.assert_called_once_with(self.file_ent,
+                                                  downloadFile=False)
+            patch_restget.assert_called_once_with('/entity/{}/accessRequirement'.format(self.file_ent.id))
