f0d4f308e657d3494faf13ec24c8cef61fb34919
Ziming Dong <ziming.dong@sagebase.org>
2020-03-18 19:14:37 -0700

    Revert "change version location"
    This reverts commit cff3428801d3bdf900fc6bc03cf2df5185127fb2.

--- synapseclient/__version__.py
+++ synapseclient/__version__.py
@@ -1,6 +1,1 @@
-import json
-
-import pkg_resources
-
-__version__ = json.load(pkg_resources.resource_stream(__name__,
-                                                       'synapsePythonClient'))['latestVersion']
+__version__ = "2.0.0.dev0"
