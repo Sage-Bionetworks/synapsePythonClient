59cb1b4e86f105e9853b905714e3867d47a9579e
Jordan Kiang <jordan.kiang@sagebase.org>
2020-04-01 08:05:46 -0700

    fix indent

--- synapseclient/core/pool_provider.py
+++ synapseclient/core/pool_provider.py
@@ -34,18 +34,18 @@ class SingleThreadExecutor(Executor):
         self._shutdown = False
 
     def submit(self, fn, *args, **kwargs):
-       if self._shutdown:
-           raise RuntimeError('cannot schedule new futures after shutdown')
+        if self._shutdown:
+            raise RuntimeError('cannot schedule new futures after shutdown')
 
-       f = Future()
-       try:
-           result = fn(*args, **kwargs)
-       except BaseException as e:
-           f.set_exception(e)
-       else:
-           f.set_result(result)
+        f = Future()
+        try:
+            result = fn(*args, **kwargs)
+        except BaseException as e:
+            f.set_exception(e)
+        else:
+            f.set_result(result)
 
-       return f
+        return f
 
 
 class FakeLock:
