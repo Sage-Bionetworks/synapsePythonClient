56fc8cf33caf6bdf65f4efcca087dd33a1df1463
Jordan Kiang <jordan.kiang@gmail.com>
2020-04-09 10:58:11 -0700

    use a separate Synapse instance in each child process, using the forked instance from the parent process can lead to connection failures due to the underlying http connection pool

--- tests/integration/__init__.py
+++ tests/integration/__init__.py
@@ -26,22 +26,40 @@ QUERY_TIMEOUT_SEC = 25
 # prior to the fork.
 _multiprocess_shared_ = True
 
+_parent_pid = os.getpid()
+_child_syn = None
+
+
+def _get_syn():
+    # return a Synapse instance for safely running tests.
+    # if we're in the parent process we can use the instance
+    # initialized during shared Setup, otherwise in a child
+    # process we lazily initialize a new Synapse.
+    # we need separate Synapse instances otherwise we may
+    # have http session issues using forked copies of the same
+    # Synapse instance and its underlying http connection pool.
+
+    if os.getpid() == _parent_pid:
+        return syn
+
+    global _child_syn
+    if not _child_syn:
+        _child_syn = _init_syn()
+    return _child_syn
+
 
 def setup_module(module):
     print("Python version:", sys.version)
 
-    syn = Synapse(debug=False, skip_checks=True)
-    syn.logger = logging.getLogger(SILENT_LOGGER_NAME)
-
+    syn = _init_syn()
     print("Testing against endpoints:")
     print("  " + syn.repoEndpoint)
     print("  " + syn.authEndpoint)
     print("  " + syn.fileHandleEndpoint)
     print("  " + syn.portalEndpoint + "\n")
 
-    syn.login()
     module.syn = syn
-    module._to_cleanup = []
+    _init_cleanup(module)
 
     # Make one project for all the tests to use
     project = syn.store(Project(name="integration_test_project"+str(uuid.uuid4())))
@@ -55,6 +73,19 @@ def setup_module(module):
     os.chdir(working_directory)
 
 
+def _init_syn():
+    syn = Synapse(debug=False, skip_checks=True)
+    syn.logger = logging.getLogger(SILENT_LOGGER_NAME)
+    syn.login()
+    return syn
+
+
+def _init_cleanup(module):
+    to_cleanup = []
+    module._to_cleanup = to_cleanup
+    module.schedule_for_cleanup = lambda item: to_cleanup.append(item)
+
+
 def init_module(module, teardown=None):
     """Instrument the given module with integration test facilities.
     Adds a logged in Synapse object, a project that can be used for
@@ -66,33 +97,28 @@ def init_module(module, teardown=None):
     :param teardown: a teardown function if there is additional behavior
         that needs to be invoked on module teardown
     """
-    module.syn = syn
+
+    module.syn = _get_syn()
     module.project = project
 
-    to_cleanup = []
-    module._to_cleanup = to_cleanup
-    module.schedule_for_cleanup = lambda item: to_cleanup.append(item)
+    _init_cleanup(module)
 
     def _teardown(module):
         if teardown:
             teardown(module)
-        cleanup(to_cleanup)
+        cleanup(module._to_cleanup, module.syn)
 
     module.teardown = _teardown
 
 
 def teardown_module(module):
     os.chdir(module._old_working_directory)
-    cleanup(module._to_cleanup)
+    cleanup(module._to_cleanup, module.syn)
 
 
-def schedule_for_cleanup(item):
-    """schedule a file of Synapse Entity to be deleted during teardown"""
-    globals()['_to_cleanup'].append(item)
-
-
-def cleanup(items):
+def cleanup(items, syn):
     """cleanup junk created during testing"""
+
     for item in reversed(items):
         if isinstance(item, Entity) or utils.is_synapse_id(item) or hasattr(item, 'deleteURI'):
             try:
