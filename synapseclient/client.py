0f1cabb4a8e34835359c8538058d6ad1688466ed
Ziming Dong <ziming.dong@sagebase.org>
2020-04-17 13:44:28 -0700

    PEP8

--- synapseclient/client.py
+++ synapseclient/client.py
@@ -972,9 +972,9 @@ class Synapse(object):
                 properties['dataFileHandleId'] = bundle['entity']['dataFileHandleId']
 
             # update the file_handle metadata if the FileEntity's FileHandle id has changed
-            local_state_fh_id = local_state.get('_file_handle',{}).get('id')
+            local_state_fh_id = local_state.get('_file_handle', {}).get('id')
             if local_state_fh_id \
-                    and properties['dataFileHandleId'] != local_state_fh_id:
+                and properties['dataFileHandleId'] != local_state_fh_id:
                 local_state['_file_handle'] = find_data_file_handle(
                     self._getEntityBundle(properties['id'], requestedObjects={'includeEntity': True,
                                                                               'includeFileHandles': True})
--- tests/unit/synapseclient/unit_test_client.py
+++ tests/unit/synapseclient/unit_test_client.py
@@ -1286,6 +1286,7 @@ def test_get_transfer_config_max_threads():
                 mock_config_dict.return_value = {'max_threads': invalid_value}
                 syn._get_transfer_config_max_threads()
 
+
 def test_store__needsUploadFalse__fileHandleId_not_in_local_state():
     returned_file_handle = {
         'id': '1234'
@@ -1299,12 +1300,12 @@ def test_store__needsUploadFalse__fileHandleId_not_in_local_state():
                        'fileHandles': [{'id': '123412341234',
                                         'concreteType': 'org.sagebionetworks.repo.model.file.S3FileHandle'}]
                        }
-    with patch.object(syn, '_getEntityBundle', return_value=returned_bundle), \
-         patch.object(synapseclient.client, 'upload_file_handle', return_value=returned_file_handle), \
-         patch.object(syn.cache, 'contains', return_value=True), \
-         patch.object(syn, '_createEntity'), \
-         patch.object(syn, 'set_annotations'), \
-         patch.object(Entity, 'create'), \
+    with patch.object(syn, '_getEntityBundle', return_value=returned_bundle),\
+         patch.object(synapseclient.client, 'upload_file_handle', return_value=returned_file_handle),\
+         patch.object(syn.cache, 'contains', return_value=True),\
+         patch.object(syn, '_createEntity'),\
+         patch.object(syn, 'set_annotations'),\
+         patch.object(Entity, 'create'),\
          patch.object(syn, 'get'):
         f = File('/fake_file.txt', parent='syn123')
         syn.store(f)
