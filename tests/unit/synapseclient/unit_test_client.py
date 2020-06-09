d20a0f1a78f85bce5d2e63d997bfd8d9ee49c460
Thomas Yu <thomasyu888@gmail.com>
2020-06-08 19:06:28 -0700

    Update tests/unit/synapseclient/unit_test_client.py

--- tests/unit/synapseclient/unit_test_client.py
+++ tests/unit/synapseclient/unit_test_client.py
@@ -1712,7 +1712,7 @@ def test__async_table_update():
                                 activity=2)
         restpost.assert_called_once_with(
             "/entity/syn1234/table/transaction/async/start",
-            body = '{"changes": [], "createSnapshot": true, '
+            body='{"changes": [], "createSnapshot": true, '
                    '"snapshotOptions": {"snapshotComment": "foo", '
                    '"snapshotLabel": "new_label", '
                    '"snapshotActivityId": 2}}'
