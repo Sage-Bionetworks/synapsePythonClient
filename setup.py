07fdca33065f1b8e4ab72cfea97499671ccb8b68
Jordan Kiang <jordan.kiang@sagebase.org>
2020-10-26 15:31:49 -0700

    add 3.9 classifier

--- setup.py
+++ setup.py
@@ -85,6 +85,7 @@ setuptools.setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Operating System :: MacOS',
         'Operating System :: Microsoft :: Windows',
         'Operating System :: Unix',
