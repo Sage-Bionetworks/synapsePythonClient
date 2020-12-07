247d13c2c987e329ff6f0c90190f28210f55d44e
Jordan Kiang <jordan.kiang@sagebase.org>
2020-12-06 17:30:05 -0800

    concrete_type_of util

--- synapseclient/core/constants/concrete_types.py
+++ synapseclient/core/constants/concrete_types.py
@@ -27,3 +27,9 @@ PARTIAL_ROW_SET = 'org.sagebionetworks.repo.model.table.PartialRowSet'
 APPENDABLE_ROWSET_REQUEST = 'org.sagebionetworks.repo.model.table.AppendableRowSetRequest'
 
 COLUMN_MODEL = 'org.sagebionetworks.repo.model.table.ColumnModel'
+
+# EntityTypes
+FILE_ENTITY = 'org.sagebionetworks.repo.model.FileEntity'
+FOLDER_ENTITY = 'org.sagebionetworks.repo.model.Folder'
+LINK_ENTITY = 'org.sagebionetworks.repo.model.Link'
+TABLE_ENTITY = 'org.sagebionetworks.repo.model.table.TableEntity'
