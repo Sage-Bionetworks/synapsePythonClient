a5b9b3db7ad136ed27d67cb75b56ba52d0ef3a45
Chia-Hui Lin <clin@SageITs-MBP.hsd1.wa.comcast.net>
2021-02-01 09:58:34 -0800

    fix pep8 style

--- synapseclient/core/upload/multipart_upload.py
+++ synapseclient/core/upload/multipart_upload.py
@@ -27,7 +27,6 @@ from typing import List, Mapping
 
 from synapseclient.core import pool_provider
 from synapseclient.core.constants import concrete_types
-from synapseclient.core.cumulative_transfer_progress import printTransferProgress
 from synapseclient.core.exceptions import (
     _raise_for_status,  # why is is this a single underscore
     SynapseHTTPError,
