1da701af078e94225475db3fc3b6440c99626511
Jordan Kiang <jordan.kiang@sagebase.org>
2021-04-09 06:49:26 -0700

    SYNPY-1134 retry AWS put part 500s during multipart upload/copy

--- synapseclient/core/upload/multipart_upload.py
+++ synapseclient/core/upload/multipart_upload.py
@@ -25,6 +25,7 @@ import threading
 import time
 from typing import List, Mapping
 
+from synapseclient.core.retry import with_retry
 from synapseclient.core import pool_provider
 from synapseclient.core.constants import concrete_types
 from synapseclient.core.exceptions import (
@@ -233,13 +234,11 @@ class UploadAttempt:
         body = self._part_request_body_provider_fn(part_number) if self._part_request_body_provider_fn else None
         part_size = len(body) if body else 0
         for retry in range(2):
+            def put_fn():
+                return session.put(part_url, body, headers=signed_headers)
             try:
-                response = session.put(
-                    part_url,
-                    body,
-                    headers=signed_headers,
-                )
-
+                # use our backoff mechanism here, we have encountered 500s on puts to AWS signed urls
+                response = with_retry(put_fn)
                 _raise_for_status(response)
 
                 # completed upload part to s3 successfully
