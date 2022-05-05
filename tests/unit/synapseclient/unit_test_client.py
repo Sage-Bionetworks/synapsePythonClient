79ac0ef363b83a10cab544b65cee5c4e4052f472
Robert Allaway <allaway@users.noreply.github.com>
2022-05-04 22:32:26 -0700

    Update unit_test_client.py

--- tests/unit/synapseclient/unit_test_client.py
+++ tests/unit/synapseclient/unit_test_client.py
@@ -998,10 +998,10 @@ def test_check_entity_restrictions__unmet_restriction_entity_project_with_downlo
         }
         entity = 'syn123'
         syn._check_entity_restrictions(bundle, entity, True)
-    mocked_warn.assert_called_with('\nThis entity has access restrictions. Please visit the web page for this entity '
-                                   '(syn.onweb("syn123")).  Look for the 'Access' label and the lock icon underneath the '
-                                   'file name. Click 'Request Access', and then review and fulfill the file download '
-                                   'requirement(s).\n')
+    mocked_warn.assert_called_with("\nThis entity has access restrictions. Please visit the web page for this entity "
+                                   "(syn.onweb("syn123")).  Look for the 'Access' label and the lock icon underneath "
+                                   "the file name. Click 'Request Access', and then review and fulfill the file "
+                                   "download requirement(s).\n")
 
 
 def test_check_entity_restrictions__unmet_restriction_entity_folder_with_downloadFile_is_True(syn):
@@ -1018,10 +1018,11 @@ def test_check_entity_restrictions__unmet_restriction_entity_folder_with_downloa
         }
         entity = 'syn123'
         syn._check_entity_restrictions(bundle, entity, True)
-    mocked_warn.assert_called_with('\nThis entity has access restrictions. Please visit the web page for this entity '
-                                   '(syn.onweb("syn123")).  Look for the 'Access' label and the lock icon underneath the '
-                                   'file name. Click 'Request Access', and then review and fulfill the file download '
-                                   'requirement(s).\n')
+    mocked_warn.assert_called_with("\nThis entity has access restrictions. Please visit the web page for this entity "
+                                   "(syn.onweb("syn123")).  Look for the 'Access' label and the lock icon underneath "
+                                   "the file name. Click 'Request Access', and then review and fulfill the file "
+                                   "download requirement(s).\n")
+
 
 
 def test_check_entity_restrictions__unmet_restriction_downloadFile_is_False(syn):
