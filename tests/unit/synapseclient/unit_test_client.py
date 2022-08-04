490371300c7c31d7cf9805d2b2b040ba7217ae7e
thomasyu888 <thomas.yu@sagebase.org>
2022-08-05 07:51:55 +0800

    Fix codeql security issues

--- tests/unit/synapseclient/unit_test_client.py
+++ tests/unit/synapseclient/unit_test_client.py
@@ -437,37 +437,35 @@ class TestDownloadFileHandle:
         """Verify we pass along Synapse auth headers when downloading from a Synapse repo hosted url"""
 
         uri = f"{self.syn.repoEndpoint}/repo/v1/entity/syn1234567/file"
-        in_destination = tempfile.mktemp()
+        with tempfile.NamedTemporaryFile(mode="w+", delete=False) as in_destination:
+            mock_credentials = mocker.patch.object(self.syn, 'credentials')
 
-        mock_credentials = mocker.patch.object(self.syn, 'credentials')
+            response = MagicMock(spec=requests.Response)
+            response.status_code = 200
+            response.headers = {}
+            mock_get = mocker.patch.object(self.syn._requests_session, 'get')
+            mock_get.return_value = response
 
-        response = MagicMock(spec=requests.Response)
-        response.status_code = 200
-        response.headers = {}
-        mock_get = mocker.patch.object(self.syn._requests_session, 'get')
-        mock_get.return_value = response
-
-        out_destination = self.syn._download_from_URL(uri, in_destination)
-        assert mock_get.call_args[1]['auth'] is mock_credentials
-        assert os.path.normpath(out_destination) == os.path.normpath(in_destination)
+            out_destination = self.syn._download_from_URL(uri, in_destination)
+            assert mock_get.call_args[1]['auth'] is mock_credentials
+            assert os.path.normpath(out_destination) == os.path.normpath(in_destination)
 
     def test_download_from_url__external(self, mocker):
         """Verify we do not pass along Synapse auth headers to a file download that is a not Synapse repo hosted"""
 
         uri = "https://not-synapse.org/foo/bar/baz"
-        in_destination = tempfile.mktemp()
+        with tempfile.NamedTemporaryFile(mode="w+", delete=False) as in_destination:
+            mocker.patch.object(self.syn, 'credentials')
 
-        mocker.patch.object(self.syn, 'credentials')
+            response = MagicMock(spec=requests.Response)
+            response.status_code = 200
+            response.headers = {}
+            mock_get = mocker.patch.object(self.syn._requests_session, 'get')
+            mock_get.return_value = response
 
-        response = MagicMock(spec=requests.Response)
-        response.status_code = 200
-        response.headers = {}
-        mock_get = mocker.patch.object(self.syn._requests_session, 'get')
-        mock_get.return_value = response
-
-        out_destination = self.syn._download_from_URL(uri, in_destination)
-        assert mock_get.call_args[1]['auth'] is None
-        assert os.path.normpath(out_destination) == os.path.normpath(in_destination)
+            out_destination = self.syn._download_from_URL(uri, in_destination)
+            assert mock_get.call_args[1]['auth'] is None
+            assert os.path.normpath(out_destination) == os.path.normpath(in_destination)
 
     @patch.object(Synapse, '_getFileHandleDownload')
     def test_downloadFileHandle_preserve_exception_info(self, mock_getFileHandleDownload):
