e862fd2e3dd81f0d63f262f50ee74c752ad04cfd
thomasyu888 <thomas.yu@sagebase.org>
2023-04-09 18:29:05 -0700

    Remove py 3.7

--- setup.py
+++ setup.py
@@ -107,7 +107,6 @@ setuptools.setup(
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
