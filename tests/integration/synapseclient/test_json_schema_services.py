429abc222e2d1bc5b75e3fe8f401e71a40debc26
thomasyu888 <thomas.yu@sagebase.org>
2023-04-10 23:34:30 -0700

    Add tests

--- tests/integration/synapseclient/test_json_schema_services.py
+++ tests/integration/synapseclient/test_json_schema_services.py
@@ -49,71 +49,80 @@ def test_json_schema_organization(js):
     assert orgs is not None
 
 
-def test_json_schema_schemas(js):
-    js_org = "a" + uuid.uuid4().hex
-    # Create, manage, and delete a JSON Schema organization
-    my_org = js.JsonSchemaOrganization(js_org)
-    my_org.create()
-    schema_name = "my.schema"
-    rint = randint(0, 100000)
-    simple_schema = {
-        "$id": "https://example.com/person.schema.json",
-        "$schema": "https://json-schema.org/draft/2020-12/schema",
-        "title": "Person",
-        "type": "object",
-        "properties": {
-            "firstName": {
-                "type": "string",
-                "description": "The person's first name."
-            },
-            "lastName": {
-                "type": "string",
-                "description": "The person's last name."
-            },
-            "age": {
-                "description": "Age in years which must be equal to or greater than zero.",
-                "type": "integer",
-                "minimum": 0
+class TestJsonSchemaSchemas():
+
+    @pytest.fixture(autouse=True)
+    def setup(self, js):
+        self.js_org = "a" + uuid.uuid4().hex
+        # Create, manage, and delete a JSON Schema organization
+        self.my_org = js.JsonSchemaOrganization(self.js_org)
+        self.my_org.create()
+        self.schema_name = "my.schema"
+        self.rint = randint(0, 100000)
+        self.simple_schema = {
+            "$id": "https://example.com/person.schema.json",
+            "$schema": "https://json-schema.org/draft/2020-12/schema",
+            "title": "Person",
+            "type": "object",
+            "properties": {
+                "firstName": {
+                    "type": "string",
+                    "description": "The person's first name."
+                },
+                "lastName": {
+                    "type": "string",
+                    "description": "The person's last name."
+                },
+                "age": {
+                    "description": "Age in years which must be equal to or greater than zero.",
+                    "type": "integer",
+                    "minimum": 0
+                }
             }
         }
-    }
-    # Create json schema
-    new_version1 = my_org.create_json_schema(simple_schema, schema_name, f"0.{rint}.1")
 
-    schemas = my_org.list_json_schemas()
-    schema1 = next(schemas)
-    schema2 = my_org.get_json_schema(schema1.name)
-    assert schema1 is schema2
-    # Manage a specific version of a JSON schema
-    versions = schema1.list_versions()
-    version1 = next(versions)
-    raw_body = version1.body
-    full_body = version1.expand()
-    assert raw_body['properties'] == simple_schema['properties']
-    assert full_body['properties'] == simple_schema['properties']
+    def teardown(self):
+        self.my_org.delete()
 
-    new_version1.delete()
-    my_org.delete()
+    def test_json_schema_schemas_org_create_schema(self):
+        # Create json schema
+        new_version = self.my_org.create_json_schema(self.simple_schema, self.schema_name, f"0.{self.rint}.1")
+        schemas = self.my_org.list_json_schemas()
+        schema1 = next(schemas)
+        schema2 = self.my_org.get_json_schema(schema1.name)
+        assert schema1 is schema2
+        # Manage a specific version of a JSON schema
+        versions = schema1.list_versions()
+        version1 = next(versions)
+        raw_body = version1.body
+        full_body = version1.expand()
+        assert raw_body['properties'] == self.simple_schema['properties']
+        assert full_body['properties'] == self.simple_schema['properties']
+        new_version.delete()
 
-    # # Create a new JSON schema version for an existing organization
-    # rint = randint(0, 100000)
-    # org_name = "bgrande.test"
-    # schema_name = "my.schema"
+    def test_json_schema_schemas_js_create_schema(self, js):
+        # Create json schema
+        # Version 2 of creating json schema
+        my_schema = js.JsonSchema(self.my_org, self.schema_name)
+        new_version = my_schema.create(self.simple_schema, f"0.{self.rint}.2")
+        schemas = self.my_org.list_json_schemas()
+        schema1 = next(schemas)
+        schema2 = self.my_org.get_json_schema(schema1.name)
+        assert schema1 is schema2
+        new_version.delete()
 
-    # # Method 1
-    # my_org          = js.JsonSchemaOrganization(org_name)
-    # new_version1    = my_org.create_json_schema(raw_body, schema_name, f"0.{rint}.1")
-
-    # # Method 2
-    # my_schema    = js.JsonSchema(my_org, schema_name)
-    # new_version2 = my_schema.create(raw_body, f"0.{rint}.2")
-
-    # # Method 3
-    # my_version   = js.JsonSchemaVersion(my_org, schema_name, f"0.{rint}.3")
-    # new_version3 = my_version.create(raw_body)
+    def test_json_schema_schemas_js_version_create_schema(self, js):
+        # Create json schema
+        # Version 3 of creating json schema
+        my_version = js.JsonSchemaVersion(self.my_org, self.schema_name, f"0.{self.rint}.3")
+        new_version = my_version.create(self.simple_schema)
+        schemas = self.my_org.list_json_schemas()
+        schema1 = next(schemas)
+        schema2 = self.my_org.get_json_schema(schema1.name)
+        assert schema1 is schema2
+        new_version.delete()
 
     # # Test validation on a Synapse entity
-    # synapse_id = "syn25922647"
     # project_name = uuid.uuid4().hex
     # project = synapseclient.Project(name=project_name)
     # project = syn.store(project)
