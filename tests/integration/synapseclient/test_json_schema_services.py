bb348416f89184403411b196106f6569f367fb74
thomasyu888 <thomas.yu@sagebase.org>
2023-06-03 12:33:55 -0700

    Lint

--- tests/integration/synapseclient/test_json_schema_services.py
+++ tests/integration/synapseclient/test_json_schema_services.py
@@ -26,31 +26,35 @@ def test_json_schema_organization(js):
     my_org = js.JsonSchemaOrganization(js_org)
     my_org.create()
     created_org = js.get_organization(js_org)
-    assert created_org['name'] == js_org
+    assert created_org["name"] == js_org
 
     original_acl = my_org.get_acl()
-    new_acl = my_org.update_acl(
-        principal_ids=[273949],
-        access_type=['READ']
+    new_acl = my_org.update_acl(principal_ids=[273949], access_type=["READ"])
+    assert len(new_acl["resourceAccess"]) == 2
+    remove_public_acl = my_org.set_acl(
+        principal_ids=[js.synapse.getUserProfile()["ownerId"]]
     )
-    assert len(new_acl['resourceAccess']) == 2
-    remove_public_acl = my_org.set_acl(principal_ids=[js.synapse.getUserProfile()['ownerId']])
     # Use set to reorder the resource access control list
-    original_acl['resourceAccess'][0]['accessType'] = set(original_acl['resourceAccess'][0]['accessType'])
-    remove_public_acl['resourceAccess'][0]['accessType'] = set(remove_public_acl['resourceAccess'][0]['accessType'])
-    assert original_acl['resourceAccess'] == remove_public_acl['resourceAccess']
+    original_acl["resourceAccess"][0]["accessType"] = set(
+        original_acl["resourceAccess"][0]["accessType"]
+    )
+    remove_public_acl["resourceAccess"][0]["accessType"] = set(
+        remove_public_acl["resourceAccess"][0]["accessType"]
+    )
+    assert original_acl["resourceAccess"] == remove_public_acl["resourceAccess"]
 
     my_org.delete()
     # the org should be deleted
-    with pytest.raises(SynapseHTTPError, match=f"Organization with name: '{js_org}' not found"):
+    with pytest.raises(
+        SynapseHTTPError, match=f"Organization with name: '{js_org}' not found"
+    ):
         js.get_organization(js_org)
     # Retrieve existing organization and associated JSON schemas
     orgs = js.list_organizations()
     assert orgs is not None
 
 
-class TestJsonSchemaSchemas():
-
+class TestJsonSchemaSchemas:
     @pytest.fixture(autouse=True)
     def setup(self, js):
         self.js_org = "a" + uuid.uuid4().hex
@@ -67,18 +71,18 @@ class TestJsonSchemaSchemas():
             "properties": {
                 "firstName": {
                     "type": "string",
-                    "description": "The person's first name."
+                    "description": "The person's first name.",
                 },
                 "lastName": {
                     "type": "string",
-                    "description": "The person's last name."
+                    "description": "The person's last name.",
                 },
                 "age": {
                     "description": "Age in years which must be equal to or greater than zero.",
                     "type": "integer",
-                    "minimum": 0
-                }
-            }
+                    "minimum": 0,
+                },
+            },
         }
 
     def teardown(self):
@@ -86,7 +90,9 @@ class TestJsonSchemaSchemas():
 
     def test_json_schema_schemas_org_create_schema(self):
         # Create json schema
-        new_version = self.my_org.create_json_schema(self.simple_schema, self.schema_name, f"0.{self.rint}.1")
+        new_version = self.my_org.create_json_schema(
+            self.simple_schema, self.schema_name, f"0.{self.rint}.1"
+        )
         schemas = self.my_org.list_json_schemas()
         schema1 = next(schemas)
         schema2 = self.my_org.get_json_schema(schema1.name)
@@ -96,8 +102,8 @@ class TestJsonSchemaSchemas():
         version1 = next(versions)
         raw_body = version1.body
         full_body = version1.expand()
-        assert raw_body['properties'] == self.simple_schema['properties']
-        assert full_body['properties'] == self.simple_schema['properties']
+        assert raw_body["properties"] == self.simple_schema["properties"]
+        assert full_body["properties"] == self.simple_schema["properties"]
         new_version.delete()
 
     def test_json_schema_schemas_js_create_schema(self, js):
@@ -114,7 +120,9 @@ class TestJsonSchemaSchemas():
     def test_json_schema_schemas_js_version_create_schema(self, js):
         # Create json schema
         # Version 3 of creating json schema
-        my_version = js.JsonSchemaVersion(self.my_org, self.schema_name, f"0.{self.rint}.3")
+        my_version = js.JsonSchemaVersion(
+            self.my_org, self.schema_name, f"0.{self.rint}.3"
+        )
         new_version = my_version.create(self.simple_schema)
         schemas = self.my_org.list_json_schemas()
         schema1 = next(schemas)
@@ -130,18 +138,20 @@ class TestJsonSchemaSchemas():
         synapse_id = project.id
         schedule_for_cleanup(project)
 
-        new_version = self.my_org.create_json_schema(self.simple_schema, self.schema_name, f"0.{self.rint}.1")
+        new_version = self.my_org.create_json_schema(
+            self.simple_schema, self.schema_name, f"0.{self.rint}.1"
+        )
         js.bind_json_schema(new_version.uri, synapse_id)
         sleep(3)
         # TODO: look into why this doesn't work
         # js.validate(synapse_id)
         js.validate_children(synapse_id)
         stats = js.validation_stats(synapse_id)
-        assert stats['containerId'] == synapse_id
-        assert stats['totalNumberOfChildren'] == 0
-        assert stats['numberOfValidChildren'] == 0
-        assert stats['numberOfInvalidChildren'] == 0
-        assert stats['numberOfUnknownChildren'] == 0
+        assert stats["containerId"] == synapse_id
+        assert stats["totalNumberOfChildren"] == 0
+        assert stats["numberOfValidChildren"] == 0
+        assert stats["numberOfInvalidChildren"] == 0
+        assert stats["numberOfUnknownChildren"] == 0
 
         js.unbind_json_schema(synapse_id)
         new_version.delete()
