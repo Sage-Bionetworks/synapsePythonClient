5b5bacfd34197169f807c60e0512ca9f97c671ca
verena <9377970+vpchung@users.noreply.github.com>
2023-06-13 17:28:01 -0700

    fix test; add another test

--- tests/unit/synapseclient/services/unit_test_json_schema.py
+++ tests/unit/synapseclient/services/unit_test_json_schema.py
@@ -1,14 +1,29 @@
 """TODO: Add more tests"""
 from synapseclient.services import json_schema
+import pytest
 
 
 def test_json_schema_organization():
     org = json_schema.JsonSchemaOrganization(
-        name="temp",
+        name="foo123",
     )
-    assert org.name == "temp"
+    assert org.name == "foo123"
     assert org.id is None
     assert org.created_on is None
     assert org.created_by is None
     assert org._json_schemas == dict()
     assert org._raw_json_schemas == dict()
+
+
+def test_json_schema_organization_bad_name():
+    with pytest.raises(ValueError):
+
+        # Name is too short
+        json_schema.JsonSchemaOrganization(
+            name="foo",
+        )
+
+        # Name cannot start with a number
+        json_schema.JsonSchemaOrganization(
+            name="123foo",
+        )
