a42e88260d1345b67c64ae64765a08084b5c93ba
thomasyu888 <thomas.yu@sagebase.org>
2023-06-26 10:37:30 -0700

    Add in specific versioning

--- setup.py
+++ setup.py
@@ -1,16 +1,7 @@
 # Installation script for Synapse Client for Python
 ############################################################
-# import sys
 import os
-
-# import platform
-import setuptools
-import json
-
-# figure out the version
-__version__ = json.loads(open("synapseclient/synapsePythonClient").read())[
-    "latestVersion"
-]
+from setuptools import setup
 
 # make sure not to overwrite existing .synapseConfig with our example one
 data_files = (
@@ -19,8 +10,4 @@ data_files = (
     else []
 )
 
-setuptools.setup(
-    # basic
-    version=__version__,
-    data_files=data_files,
-)
+setup(data_files=data_files)
