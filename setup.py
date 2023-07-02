a2f2055e1c03a10dd9c14caaab4708dcea20f2ae
thomasyu888 <thomas.yu@sagebase.org>
2023-07-02 13:39:22 -0700

    Don't move version into pyproject.toml yet

--- setup.py
+++ setup.py
@@ -3,11 +3,16 @@
 import os
 from setuptools import setup
 
+import json
+
 # make sure not to overwrite existing .synapseConfig with our example one
 data_files = (
     [(os.path.expanduser("~"), ["synapseclient/.synapseConfig"])]
     if not os.path.exists(os.path.expanduser("~/.synapseConfig"))
     else []
 )
+# figure out the version
+with open("synapseclient/synapsePythonClient") as config:
+    __version__ = json.load(config)["latestVersion"]
 
-setup(data_files=data_files)
+setup(data_files=data_files, version=__version__)
