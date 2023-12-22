2653bdaa005c2be55983d9b3f9feff477040a2ba
Jenny Medina <medina.stsci@gmail.com>
2023-12-22 13:50:04 -0500

    Added indent to previous example so imports show up

--- synapseclient/annotations.py
+++ synapseclient/annotations.py
@@ -149,6 +149,7 @@ def to_submission_status_annotations(annotations, is_private=True):
 
     Example: Using this function
         Adding and converting annotations
+
             from synapseclient.client import Synapse
             from synapseclient.annotations import to_submission_status_annotations
             from datetime import datetime as Datetime
