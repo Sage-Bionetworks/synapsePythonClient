3ebada8c6ad72d4cef1fc429eb514f0d54cff8d6
Brad Macdonald <52762200+BWMac@users.noreply.github.com>
2023-12-29 10:19:10 -0700

    Update synapseclient/entity.py
    Co-authored-by: BryanFauble <17128019+BryanFauble@users.noreply.github.com>

--- synapseclient/entity.py
+++ synapseclient/entity.py
@@ -806,7 +806,7 @@ class DockerRepository(Entity):
     NOTE: [store()][synapseclient.Synapse.store]-ing a DockerRepository created in the Python client will always result
     in it being treated as a reference to an external Docker repository that is not
     managed by synapse. To upload a docker image that is managed by Synapse please use the official
-    Docker client and read https://help.synapse.org/docs/Synapse-Docker-Registry.2011037752.html
+    Docker client and read <https://help.synapse.org/docs/Synapse-Docker-Registry.2011037752.html>
     for instructions on uploading a Docker Image to Synapse
 
     Attributes:
