bf22e7f0eb0e9879c190cbe47add0afad831ab8c
Brad Macdonald <bwm03570@gmail.com>
2023-12-29 10:43:50 -0700

    indents `itersubclasses` docstring for code block

--- synapseclient/core/utils.py
+++ synapseclient/core/utils.py
@@ -599,23 +599,23 @@ def itersubclasses(cls, _seen=None):
 
     Generator over all subclasses of a given class, in depth first order.
 
-    >>> list(itersubclasses(int)) == [bool]
-    True
-    >>> class A(object): pass
-    >>> class B(A): pass
-    >>> class C(A): pass
-    >>> class D(B,C): pass
-    >>> class E(D): pass
-    >>>
-    >>> for cls in itersubclasses(A):
-    ...     print(cls.__name__)
-    B
-    D
-    E
-    C
-    >>> # get ALL (new-style) classes currently defined
-    >>> [cls.__name__ for cls in itersubclasses(object)] #doctest: +ELLIPSIS
-    ['type', ...'tuple', ...]
+        >>> list(itersubclasses(int)) == [bool]
+        True
+        >>> class A(object): pass
+        >>> class B(A): pass
+        >>> class C(A): pass
+        >>> class D(B,C): pass
+        >>> class E(D): pass
+        >>>
+        >>> for cls in itersubclasses(A):
+        ...     print(cls.__name__)
+        B
+        D
+        E
+        C
+        >>> # get ALL (new-style) classes currently defined
+        >>> [cls.__name__ for cls in itersubclasses(object)] #doctest: +ELLIPSIS
+        ['type', ...'tuple', ...]
     """
 
     if not isinstance(cls, type):
