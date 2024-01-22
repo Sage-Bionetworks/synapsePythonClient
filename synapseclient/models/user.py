dcb43d3e727b4d175847fd9e1c659ad29b53d6ff
Brad Macdonald <bwm03570@gmail.com>
2024-01-22 15:47:06 -0700

    adds UserGroupHeader to user.py

---
+++ synapseclient/models/user.py
@@ -0,0 +1,49 @@
+from dataclasses import dataclass
+from typing import Optional
+
+from synapseclient.team import UserGroupHeader as Synapse_UserGroupHeader
+
+
+@dataclass
+class UserGroupHeader:
+    """
+    Select metadata about a Synapse principal.
+    In practice the constructor is not called directly by the client.
+
+    Attributes:
+        owner_id: A foreign key to the ID of the 'principal' object for the user.
+        first_name: First Name
+        last_name: Last Name
+        user_name: A name chosen by the user that uniquely identifies them.
+        email: User's current email address
+        is_individual: True if this is a user, false if it is a group
+    """
+
+    owner_id: int
+    """A foreign key to the ID of the 'principal' object for the user."""
+
+    first_name: str
+    """First Name"""
+
+    last_name: str
+    """Last Name"""
+
+    user_name: str
+    """A name chosen by the user that uniquely identifies them."""
+
+    is_individual: bool
+    """True if this is a user, false if it is a group"""
+
+    email: Optional[str] = None
+    """User's current email address"""
+
+    def fill_from_dict(
+        self, synapse_user_group_header: Synapse_UserGroupHeader
+    ) -> "UserGroupHeader":
+        self.owner_id = synapse_user_group_header.get("ownerId", None)
+        self.first_name = synapse_user_group_header.get("firstName", None)
+        self.last_name = synapse_user_group_header.get("lastName", None)
+        self.user_name = synapse_user_group_header.get("userName", None)
+        self.email = synapse_user_group_header.get("email", None)
+        self.is_individual = synapse_user_group_header.get("isIndividual", None)
+        return self
