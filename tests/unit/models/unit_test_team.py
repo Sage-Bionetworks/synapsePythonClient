10352aef5e7b93ed5bba4b0cb173db9704290a14
Brad Macdonald <bwm03570@gmail.com>
2024-01-26 13:40:44 -0700

    creates constants for unit tests

--- tests/unit/models/unit_test_team.py
+++ tests/unit/models/unit_test_team.py
@@ -20,37 +20,50 @@ class TestTeamMember:
 
 class TestTeam:
     @pytest.fixture(autouse=True, scope="function")
-    def init_syn(self, syn: Synapse):
+    def init(self, syn: Synapse):
         self.syn = syn
+        self.MESSAGE = "test message"
+        self.USER = "test_user"
+        self.DESCRIPTION = "test description"
+        self.NAME = "test_team"
+        self.TIMESTAMP = "2020-01-01T00:00:00.000Z"
+        self.invite_response = {
+            "id": "1",
+            "teamId": "2",
+            "inviteeId": "3",
+            "message": self.MESSAGE,
+            "createdOn": "2000-01-01T00:00:00.000Z",
+            "createdBy": "4",
+        }
 
     def test_fill_from_dict(self) -> None:
         team = Team()
         team.fill_from_dict(
             synapse_team={
                 "id": "1",
-                "name": "test_team",
-                "description": "test description",
+                "name": self.NAME,
+                "description": self.DESCRIPTION,
                 "icon": "test_file_handle_id",
                 "canPublicJoin": True,
                 "canRequestMembership": True,
                 "etag": "11111111-1111-1111-1111-111111111111",
-                "createdOn": "2020-01-01T00:00:00.000Z",
-                "modifiedOn": "2020-01-01T00:00:00.000Z",
-                "createdBy": "test_user",
-                "modifiedBy": "test_user",
+                "createdOn": self.TIMESTAMP,
+                "modifiedOn": self.TIMESTAMP,
+                "createdBy": self.USER,
+                "modifiedBy": self.USER,
             }
         )
         assert team.id == 1
-        assert team.name == "test_team"
-        assert team.description == "test description"
+        assert team.name == self.NAME
+        assert team.description == self.DESCRIPTION
         assert team.icon == "test_file_handle_id"
         assert team.can_public_join is True
         assert team.can_request_membership is True
         assert team.etag == "11111111-1111-1111-1111-111111111111"
-        assert team.created_on == "2020-01-01T00:00:00.000Z"
-        assert team.modified_on == "2020-01-01T00:00:00.000Z"
-        assert team.created_by == "test_user"
-        assert team.modified_by == "test_user"
+        assert team.created_on == self.TIMESTAMP
+        assert team.modified_on == self.TIMESTAMP
+        assert team.created_by == self.USER
+        assert team.modified_by == self.USER
 
     @pytest.mark.asyncio
     async def test_create(self) -> None:
@@ -58,28 +71,28 @@ class TestTeam:
             self.syn,
             "create_team",
             return_value=Synapse_Team(
-                id=1, name="test_team", description="test description"
+                id=1, name=self.NAME, description=self.DESCRIPTION
             ),
         ) as patch_create_team:
-            team = Team(name="test_team", description="test description")
+            team = Team(name=self.NAME, description=self.DESCRIPTION)
             team = await team.create()
             patch_create_team.assert_called_once_with(
-                name="test_team",
-                description="test description",
+                name=self.NAME,
+                description=self.DESCRIPTION,
                 icon=None,
                 opentelemetry_context={},
             )
             assert team.id == 1
-            assert team.name == "test_team"
+            assert team.name == self.NAME
 
     @pytest.mark.asyncio
     async def test_delete(self) -> None:
         with patch.object(
             self.syn,
             "delete_team",
-            return_value=Synapse_Team(id=1, name="test_team"),
+            return_value=Synapse_Team(id=1, name=self.NAME),
         ) as patch_delete_team:
-            team = Team(id=1, name="test_team")
+            team = Team(id=1, name=self.NAME)
             await team.delete()
             patch_delete_team.assert_called_once_with(id=1, opentelemetry_context={})
 
@@ -89,13 +102,13 @@ class TestTeam:
             self.syn,
             "getTeam",
             return_value=Synapse_Team(
-                id=1, name="test_team", description="test description"
+                id=1, name=self.NAME, description=self.DESCRIPTION
             ),
         ) as patch_get_team:
             team = await Team().from_id(id=1)
             patch_get_team.assert_called_once_with(id=1, opentelemetry_context={})
             assert team.id == 1
-            assert team.name == "test_team"
+            assert team.name == self.NAME
 
     @pytest.mark.asyncio
     async def test_from_name(self) -> None:
@@ -103,15 +116,15 @@ class TestTeam:
             self.syn,
             "getTeam",
             return_value=Synapse_Team(
-                id=1, name="test_team", description="test description"
+                id=1, name=self.NAME, description=self.DESCRIPTION
             ),
         ) as patch_get_team:
-            team = await Team().from_name(name="test_team")
+            team = await Team().from_name(name=self.NAME)
             patch_get_team.assert_called_once_with(
-                id="test_team", opentelemetry_context={}
+                id=self.NAME, opentelemetry_context={}
             )
             assert team.id == 1
-            assert team.name == "test_team"
+            assert team.name == self.NAME
 
     @pytest.mark.asyncio
     async def test_members(self) -> None:
@@ -130,47 +143,31 @@ class TestTeam:
 
     @pytest.mark.asyncio
     async def test_invite(self) -> None:
-        response_body = {
-            "id": "1",
-            "teamId": "2",
-            "inviteeId": "3",
-            "message": "test message",
-            "createdOn": "2000-01-01T00:00:00.000Z",
-            "createdBy": "4",
-        }
         with patch.object(
             self.syn,
             "invite_to_team",
-            return_value=response_body,
+            return_value=self.invite_response,
         ) as patch_invite_team_member:
             team = Team(id=1)
             invite = await team.invite(
-                user="test_user",
-                message="test message",
+                user=self.USER,
+                message=self.MESSAGE,
             )
             patch_invite_team_member.assert_called_once_with(
                 team=team,
-                user="test_user",
-                message="test message",
+                user=self.USER,
+                message=self.MESSAGE,
                 force=True,
                 opentelemetry_context={},
             )
-            assert invite == response_body
+            assert invite == self.invite_response
 
     @pytest.mark.asyncio
     async def test_open_invitations(self) -> None:
-        response_body = {
-            "id": "1",
-            "teamId": "2",
-            "inviteeId": "3",
-            "message": "test message",
-            "createdOn": "2000-01-01T00:00:00.000Z",
-            "createdBy": "4",
-        }
         with patch.object(
             self.syn,
             "get_team_open_invitations",
-            return_value=[response_body],
+            return_value=[self.invite_response],
         ) as patch_get_open_team_invitations:
             team = Team(id=1)
             open_team_invitations = await team.open_invitations()
@@ -178,4 +175,4 @@ class TestTeam:
                 team=team, opentelemetry_context={}
             )
             assert len(open_team_invitations) == 1
-            assert open_team_invitations[0] == response_body
+            assert open_team_invitations[0] == self.invite_response
