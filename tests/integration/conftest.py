40c8f63e0887d2c8567c13b4a2f3fd9c3a4a31a2
BryanFauble <17128019+BryanFauble@users.noreply.github.com>
2024-04-03 16:41:35 -0700

    Replace period with nothing

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -235,7 +235,9 @@ def set_timezone():
 def alternative_uuid_generation() -> str:
     """Alternative UUID generation function that includes the system timestamp."""
     timestamp = str(os.times()[4])
-    return str(f"{uuid.UUID(bytes=os.urandom(16), version=4)}-{timestamp}")
+    return str(f"{uuid.UUID(bytes=os.urandom(16), version=4)}-{timestamp}").replace(
+        ".", ""
+    )
 
 
 @pytest.fixture(scope="session", autouse=True)
