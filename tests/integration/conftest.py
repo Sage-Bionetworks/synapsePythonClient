f72b6e9af5a338cfd87c0e6364666007eec06af2
BryanFauble <17128019+BryanFauble@users.noreply.github.com>
2024-04-03 18:24:17 -0700

    a

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -233,7 +233,9 @@ def set_timezone():
 
 
 def alternative_uuid_generation() -> str:
-    """Alternative UUID generation function that includes the system timestamp."""
+    """
+    Alternative UUID generation function that includes the system timestamp.
+    """
     timestamp = str(time.time())
     return str(f"{uuid.UUID(bytes=os.urandom(16), version=4)}-{timestamp}").replace(
         ".", ""
