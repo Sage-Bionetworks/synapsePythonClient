c4f76b836777cbc250bd5587a06d01318b557123
BryanFauble <17128019+BryanFauble@users.noreply.github.com>
2024-09-06 16:35:10 -0700

    [FDS-2373] Update retry delay, add some otel traces, add retry to url creation (#1128)
    * Update retry delay, add some otel traces, add retry to url creation

--- synapseclient/core/dozer.py
+++ synapseclient/core/dozer.py
@@ -7,6 +7,10 @@ sleep while checking registered _listeners
 """
 import time
 
+from opentelemetry import trace
+
+tracer = trace.get_tracer("synapseclient")
+
 _listeners = []
 
 
@@ -20,9 +24,21 @@ def clear_listeners():
     del _listeners[:]
 
 
-def doze(secs, listener_check_interval_secs=0.1):
-    end_time = time.time() + secs
-    while time.time() < end_time:
-        for listener in _listeners:
-            listener()
-        time.sleep(listener_check_interval_secs)
+def doze(
+    secs: float,
+    listener_check_interval_secs: float = 0.1,
+    trace_span_name: str = "doze",
+) -> None:
+    """Sleep for a given number of seconds while checking registered listeners.
+
+    Arguments:
+        secs: the number of seconds to sleep
+        listener_check_interval_secs: the interval at which to check the listeners
+        trace_span_name: the name of the trace span
+    """
+    with tracer.start_as_current_span(name=trace_span_name):
+        end_time = time.time() + secs
+        while time.time() < end_time:
+            for listener in _listeners:
+                listener()
+            time.sleep(listener_check_interval_secs)
