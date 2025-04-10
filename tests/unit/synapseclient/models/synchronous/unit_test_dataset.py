2d4b8e6fb3c770e6f7077fc6af6804d20daf5467
BWMac <bwm03570@gmail.com>
2025-04-10 11:52:40 -0400

    adds unit tests

--- tests/unit/synapseclient/models/synchronous/unit_test_dataset.py
+++ tests/unit/synapseclient/models/synchronous/unit_test_dataset.py
@@ -4,8 +4,7 @@ import pytest
 
 from synapseclient import Synapse
 from synapseclient.core.constants import concrete_types
-from synapseclient.models import Dataset, EntityRef, File, Folder
-
+from synapseclient.models import Dataset, DatasetCollection, EntityRef, File, Folder
 
 class TestDataset:
     @pytest.fixture(autouse=True, scope="function")
@@ -91,3 +90,50 @@ class TestDataset:
             dataset.remove_item(Folder(id="syn1234"))
         # THEN I expect the Dataset to have no items
         assert dataset.items == []
+
+class TestDatasetCollection:
+    @pytest.fixture(autouse=True, scope="function")
+    def init_syn(self, syn: Synapse) -> None:
+        self.syn = syn
+
+    def test_add_item_dataset(self):
+        # GIVEN an empty DatasetCollection
+        dataset_collection = DatasetCollection()
+        # WHEN I add a Dataset to it
+        dataset_collection.add_item(Dataset(id="syn1234", version_number=1))
+        # THEN I expect the DatasetCollection to have the Dataset in its items
+        assert dataset_collection.items == [EntityRef(id="syn1234", version=1)]
+
+    def test_add_item_entity_ref(self):
+        # GIVEN an empty DatasetCollection
+        dataset_collection = DatasetCollection()
+        # WHEN I add an EntityRef to it
+        dataset_collection.add_item(EntityRef(id="syn1234", version=1))
+        # THEN I expect the DatasetCollection to have the EntityRef in its items
+        assert dataset_collection.items == [EntityRef(id="syn1234", version=1)]
+
+    def test_remove_item_invalid_type(self):
+        # GIVEN an empty DatasetCollection
+        dataset_collection = DatasetCollection()
+        # WHEN I add an invalid type to it
+        with pytest.raises(
+            ValueError,
+            match="item must be a Dataset or EntityRef. 1 is a <class 'int'>",
+        ):
+            dataset_collection.add_item(1)
+
+    def test_remove_item_dataset(self):
+        # GIVEN a DatasetCollection with a Dataset
+        dataset_collection = DatasetCollection(items=[EntityRef(id="syn1234", version=1)])
+        # WHEN I remove the Dataset from it
+        dataset_collection.remove_item(Dataset(id="syn1234", version_number=1))
+        # THEN I expect the DatasetCollection to have no items
+        assert dataset_collection.items == []
+
+    def test_remove_item_entity_ref(self):
+        # GIVEN a DatasetCollection with an EntityRef
+        dataset_collection = DatasetCollection(items=[EntityRef(id="syn1234", version=1)])
+        # WHEN I remove the EntityRef from it
+        dataset_collection.remove_item(EntityRef(id="syn1234", version=1))
+        # THEN I expect the DatasetCollection to have no items
+        assert dataset_collection.items == []
