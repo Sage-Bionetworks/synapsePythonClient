2ffdeabdfb147c4365c81e9905e7166435130da1
BWMac <bwm03570@gmail.com>
2025-04-11 15:25:30 -0400

    fixes tutorial script

--- docs/tutorials/python/tutorial_scripts/dataset_collection.py
+++ docs/tutorials/python/tutorial_scripts/dataset_collection.py
@@ -3,7 +3,7 @@
 import pandas as pd
 
 from synapseclient import Synapse
-from synapseclient.models import Dataset, DatasetCollection, Project, Column, ColumnType
+from synapseclient.models import Column, ColumnType, Dataset, DatasetCollection, Project
 
 # First, let's get the project that we want to create the DatasetCollection in
 syn = Synapse()
@@ -20,11 +20,17 @@ print(project_id)
 # Let's create the DatasetCollection. We'll use the project id as the parent id.
 # At first, the DatasetCollection will be empty, but if you view the DatasetCollection's schema in the UI,
 # you will notice that DatasetCollections come with default columns.
-DATASET_IDS = ["syn65987017", "syn65987019", "syn65987020"] # Replace with your dataset IDs
-test_dataset_collection = DatasetCollection(parent_id=project_id, name="test_dataset_collection").store()
+DATASET_IDS = [
+    "syn65987017",
+    "syn65987019",
+    "syn65987020",
+]  # Replace with your dataset IDs
+test_dataset_collection = DatasetCollection(
+    parent_id=project_id, name="test_dataset_collection"
+).store()
 print(f"My DatasetCollection's ID is {test_dataset_collection.id}")
 
-# Now, let's add some datasets to the collection. We will loop through our dataset ids and add each dataset to the 
+# Now, let's add some datasets to the collection. We will loop through our dataset ids and add each dataset to the
 # collection using the `add_item` method.
 for dataset_id in DATASET_IDS:
     test_dataset_collection.add_item(Dataset(id=dataset_id).get())
@@ -35,7 +41,7 @@ test_dataset_collection.store()
 # we can retrieve it from Synapse.
 my_retrieved_dataset_collection = DatasetCollection(id=test_dataset_collection.id).get()
 print(f"My DatasetCollection's ID is still {my_retrieved_dataset_collection.id}")
-print(len(my_retrieved_dataset_collection.items))
+print(f"My DatasetCollection has {len(my_retrieved_dataset_collection.items)} items")
 
 # In addition to the default columns, you may want to annotate items in your DatasetCollection using
 # custom columns.
@@ -49,9 +55,14 @@ my_retrieved_dataset_collection.store()
 
 # Now that our custom column has been added, we can update the DatasetCollection with new annotations.
 modified_data = pd.DataFrame(
-    {"id": DATASET_IDS, "my_annotation": ["good dataset", "better dataset", "best dataset"]}
+    {
+        "id": DATASET_IDS,
+        "my_annotation": ["good dataset", "better dataset", "best dataset"],
+    }
 )
-my_retrieved_dataset_collection.update_rows(values=modified_data, primary_keys=["id"], dry_run=False)
+my_retrieved_dataset_collection.update_rows(
+    values=modified_data, primary_keys=["id"], dry_run=False
+)
 
 # If you want to query your DatasetCollection for items that match certain criteria, you can do so
 # using the `query` method.
