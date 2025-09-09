beceb39e882023483e267e4a37dae5d5457c6c5d
Thomas Yu <thomas.yu@sagebase.org>
2025-09-09 01:06:26 -0700

    Update walk solution

--- docs/tutorials/python/tutorial_scripts/file.py
+++ docs/tutorials/python/tutorial_scripts/file.py
@@ -12,67 +12,65 @@ from synapseclient.models import File, Folder, Project
 syn = synapseclient.login()
 
 # Retrieve the project ID
-my_project_id = (
-    Project(name="My uniquely named project about Alzheimer's Disease").get().id
-)
+my_project = Project(name="My uniquely named project about Alzheimer's Disease").get()
 
 # Retrieve the IDs of the folders I want to upload to
-batch_1_folder = (
-    Folder(parent_id=my_project_id, name="single_cell_RNAseq_batch_1").get().id
-)
-batch_2_folder = (
-    Folder(parent_id=my_project_id, name="single_cell_RNAseq_batch_2").get().id
-)
-biospecimen_experiment_1_folder = (
-    Folder(parent_id=my_project_id, name="biospecimen_experiment_1").get().id
-)
-biospecimen_experiment_2_folder = (
-    Folder(parent_id=my_project_id, name="biospecimen_experiment_2").get().id
-)
+batch_1_folder = Folder(
+    parent_id=my_project.id, name="single_cell_RNAseq_batch_1"
+).get()
+batch_2_folder = Folder(
+    parent_id=my_project.id, name="single_cell_RNAseq_batch_2"
+).get()
+biospecimen_experiment_1_folder = Folder(
+    parent_id=my_project.id, name="biospecimen_experiment_1"
+).get()
+biospecimen_experiment_2_folder = Folder(
+    parent_id=my_project.id, name="biospecimen_experiment_2"
+).get()
 
 # Create a File object for each file I want to upload
 biospecimen_experiment_1_a_2022 = File(
     path=os.path.expanduser("~/my_ad_project/biospecimen_experiment_1/fileA.txt"),
-    parent_id=biospecimen_experiment_1_folder,
+    parent_id=biospecimen_experiment_1_folder.id,
 )
 biospecimen_experiment_1_b_2022 = File(
     path=os.path.expanduser("~/my_ad_project/biospecimen_experiment_1/fileB.txt"),
-    parent_id=biospecimen_experiment_1_folder,
+    parent_id=biospecimen_experiment_1_folder.id,
 )
 
 biospecimen_experiment_2_c_2023 = File(
     path=os.path.expanduser("~/my_ad_project/biospecimen_experiment_2/fileC.txt"),
-    parent_id=biospecimen_experiment_2_folder,
+    parent_id=biospecimen_experiment_2_folder.id,
 )
 biospecimen_experiment_2_d_2023 = File(
     path=os.path.expanduser("~/my_ad_project/biospecimen_experiment_2/fileD.txt"),
-    parent_id=biospecimen_experiment_2_folder,
+    parent_id=biospecimen_experiment_2_folder.id,
 )
 
 batch_1_scrnaseq_file_1 = File(
     path=os.path.expanduser(
         "~/my_ad_project/single_cell_RNAseq_batch_1/SRR12345678_R1.fastq.gz"
     ),
-    parent_id=batch_1_folder,
+    parent_id=batch_1_folder.id,
 )
 batch_1_scrnaseq_file_2 = File(
     path=os.path.expanduser(
         "~/my_ad_project/single_cell_RNAseq_batch_1/SRR12345678_R2.fastq.gz"
     ),
-    parent_id=batch_1_folder,
+    parent_id=batch_1_folder.id,
 )
 
 batch_2_scrnaseq_file_1 = File(
     path=os.path.expanduser(
         "~/my_ad_project/single_cell_RNAseq_batch_2/SRR12345678_R1.fastq.gz"
     ),
-    parent_id=batch_2_folder,
+    parent_id=batch_2_folder.id,
 )
 batch_2_scrnaseq_file_2 = File(
     path=os.path.expanduser(
         "~/my_ad_project/single_cell_RNAseq_batch_2/SRR12345678_R2.fastq.gz"
     ),
-    parent_id=batch_2_folder,
+    parent_id=batch_2_folder.id,
 )
 
 # Upload each file to Synapse
@@ -100,13 +98,9 @@ print(
 print(f"My file was last modified on: {batch_1_scrnaseq_file_1.modified_on}")
 
 # Step 3: List all Folders and Files within my project
-for directory_path, directory_names, file_name in synapseutils.walk(
-    syn=syn, synId=my_project_id, includeTypes=["file"]
-):
-    for directory_name in directory_names:
-        print(
-            f"Directory ({directory_name[1]}): {directory_path[0]}/{directory_name[0]}"
-        )
-
-    for file in file_name:
-        print(f"File ({file[1]}): {directory_path[0]}/{file[0]}")
+my_project.sync_from_synapse(download_file=False)
+dir_mapping = my_project.map_directory_to_all_contained_files("./")
+for directory_name, file_entities in dir_mapping.items():
+    print(f"Directory: {directory_name}")
+    for file_entity in file_entities:
+        print(f"\tFile: {file_entity.name}, ID: {file_entity.id}")
