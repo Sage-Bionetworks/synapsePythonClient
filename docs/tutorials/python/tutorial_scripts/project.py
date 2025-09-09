233d6adb669d3f2b6a542260b988b02eaead0800
Thomas Yu <thomas.yu@sagebase.org>
2025-09-09 01:09:27 -0700

    Update command

--- docs/tutorials/python/tutorial_scripts/project.py
+++ docs/tutorials/python/tutorial_scripts/project.py
@@ -22,8 +22,7 @@ print(f"The ID of the user that created my project is: {project.created_by}")
 print(f"My project was last modified on: {project.modified_on}")
 
 # Step 3: Get an existing project
-my_project_id = syn.findEntityId(
+my_project_object = Project(
     name="My uniquely named project about Alzheimer's Disease"
-)
-my_project_object = syn.get(entity=my_project_id)
-print(f"I just got my project: {my_project_object.name}, id: {my_project_id}")
+).get()
+print(f"I just got my project: {my_project_object.name}, id: {my_project_object.id}")
