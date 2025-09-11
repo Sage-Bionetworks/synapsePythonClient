ff145d7e95e0ac64b5df530791243da860e62fec
Jenny Medina <medina.stsci@gmail.com>
2025-09-11 10:39:45 -0400

    Initialize protocol class + Evaluation dataclass

--- synapseclient/models/evaluation.py
+++ synapseclient/models/evaluation.py
@@ -1,1 +1,40 @@
-# TODO
+class EvaluationSynchronousProtocol(Protocol):
+    """
+    This is the protocol for methods that are asynchronous
+    but also have a synchronous counterpart that may also be called.
+    """
+
+@dataclass
+@async_to_sync
+class Evaluation(EvaluationSynchronousProtocol):
+    """TODO"""
+
+    id: Optional[str] = None
+    """The unique immutable ID for this Evaluation."""
+
+    etag: Optional[str] = None
+    """Synapse employs an Optimistic Concurrency Control (OCC) scheme to handle concurrent updates.
+    The eTag changes every time an Evaluation is updated; it is used to detect when a client's copy
+    of an Evaluation is out-of-date."""
+
+    name: Optional[str] = None
+    """The name of this Evaluation."""
+
+    description: Optional[str] = None
+    """A text description of this Evaluation."""
+
+    owner_id: Optional[str] = None
+    """The ID of the Synapse user who created this Evaluation."""
+
+    created_on: Optional[str] = None
+    """The date on which Evaluation was created."""
+
+    content_source: Optional[str] = None
+    """The Synapse ID of the Entity to which this Evaluation belongs,
+    e.g. a reference to a Synapse project."""
+
+    submission_instructions_message: Optional[str] =  None
+    """Message to display to users detailing acceptable formatting for Submissions to this Evaluation."""
+
+    submission_receipt_message: Optional[str] = None
+    """Message to display to users upon successful submission to this Evaluation."""
