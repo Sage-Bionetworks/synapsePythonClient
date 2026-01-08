2b44e52c78aeb02a2446bc95d0f2ded2a621f18d
Jenny V Medina <jenny.medina@sagebase.org>
2026-01-08 11:23:13 -0500

    [SYNPY-1590] Add `docker_commit_services` module to codebase (#1304)
    * add docker_commit_services and get_docker_tag to init

---
+++ synapseclient/api/docker_commit_services.py
@@ -0,0 +1,28 @@
+"""This module is responsible for exposing the services defined at:
+<https://rest-docs.synapse.org/rest/#org.sagebionetworks.repo.web.controller.DockerCommitController>
+"""
+from typing import TYPE_CHECKING, Optional
+
+if TYPE_CHECKING:
+    from synapseclient import Synapse
+
+
+async def get_docker_tag(
+    entity_id: str, *, synapse_client: Optional["Synapse"] = None
+) -> dict:
+    """
+    Arguments:
+        entity_id: The ID of the Docker repository entity.
+        synapse_client: If not passed in and caching was not disabled by
+                `Synapse.allow_client_caching(False)` this will use the last created
+                instance from the Synapse class constructor.
+
+    Returns:
+        The Docker tag information for the entity.
+    """
+    from synapseclient import Synapse
+
+    client = Synapse.get_client(synapse_client=synapse_client)
+    return await client.rest_get_async(
+        uri=f"/entity/{entity_id}/dockerTag",
+    )
